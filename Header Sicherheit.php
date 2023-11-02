49d4b57283b1f1c5b2fcc33ce946f94a50dd63a8
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-02 10:52:41 +0100

    Header

--- Header Sicherheit.php
+++ Header Sicherheit.php
@@ -22,3 +22,21 @@ if(isset($_SESSION["Benutzername"])) {
 }else{
     echo "<li class='li'><a href='../Login/login.php'>Login</a></li";
 }
+    $statement = $pdo -> prepare ("SELECT Profilbild FROM User WHERE ID= :USER _ID ");
+                        $statement -> bindParam(":User_ID", $_SESSION["User_ID"]);
+                        if($statement -> execute()) {
+                            if ($row = $statement->fetch()) {
+                                if (($row["Profilbild"]) == null or "") {
+                                    echo "<div>kein Profilbild</div>";
+                                } else {
+                                    echo "<div><img class='profilpicture' src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $row['Profilbild'] ."'></div>";
+                                }
+                            }
+
+                    }
+                    ?>
+        </div>
+
+        </div>
+    </header>
+    </body>
