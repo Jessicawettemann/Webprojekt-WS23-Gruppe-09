0fdb4441a68816cf97685a4c0c5e414c592cc293
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-02 11:03:14 +0100

    Verknüpfung2

--- Profil_do.php
+++ Profil_do.php
@@ -4,3 +4,14 @@ include "./Profil_do.php";
 session_start();
 
 ?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Profil bearbeiten</title>
+
+</head>
+<body>
+
+<h1> Profil bearbeiten </h1>
