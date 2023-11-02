22f36b7e52947f2c04a80aa2f7c24dd7cfb5354d
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-02 10:00:49 +0100

    Header

--- Header Sicherheit.php
+++ Header Sicherheit.php
@@ -2,3 +2,23 @@
 include "Datenbank Verbindung.php";
 session_start();
 ?>
+    <!DOCTYPE html>
+    <html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+
+    </head>
+    <body>
+    <header>
+        <div class="header">
+
+
+
+<?php
+#wenn Nutzer angemeldet ist wird zum Logout verlinkt, anderenfalls zum Login
+if(isset($_SESSION["Benutzername"])) {
+    echo "<li class='li'><a href='../Login/logout.php'>Logout</a></li"; # Login verlinken
+}else{
+    echo "<li class='li'><a href='../Login/login.php'>Login</a></li";
+}
