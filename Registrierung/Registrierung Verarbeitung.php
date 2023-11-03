b62cde7226b7460b4c70bffbda63c2a54d2ea3f1
Alina Kell <alina.kell41@gmail.com>
2023-11-03 20:38:45 +0100

    Update Registrierung Verarbeitung.php

--- Registrierung/Registrierung Verarbeitung.php
+++ Registrierung/Registrierung Verarbeitung.php
@@ -1,18 +1,26 @@
 <?php
-//Verbingsinformationen
-$hostname = "mars.iuk.hdm-stuttgart.de"; // Hostname Datenbank
-$username = "jw170"; // Datenbank-Benutzername
-$password = "Air8aing3r"; // Datenbank-Passwort
-$dbname = "u-jw170"; // Name  Datenbank
+include "Datenbank Verbindung.php"; //Datenbankverbindung herstellen
 
-try {
-    // Erstelle eine PDO-Verbindung zur Datenbank
-    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+//Sie müssen die Daten abrufen, die der Benutzer im Registrierungsformular eingegeben hat. 
+$vorname = $_POST['vorname'];
+$nachname = $_POST['nachname'];
+$email = $_POST['email'];
+$benutzername = $_POST['benutzername'];
+$passwort = $_POST['passwort'];
 
-    echo "Verbindung zur Datenbank erfolgreich hergestellt!";
-} catch (PDOException $e) {
-    die("Verbindung zur Datenbank fehlgeschlagen: " . $e->getMessage());
-}
-?>
+//Erstellen Sie einen SQL-Befehl, um die Daten in die Datenbank einzufügen.
+$sql = "INSERT INTO User (Vorname, Nachname, EMail, Benutzername, Passwort) VALUES (:vorname, :nachname, :email, :benutzername, :passwort)";
 
+//Verwenden Sie PDO, um den vorbereiteten SQL-Befehl auszuführen.
+// Binden Sie die Parameter an die entsprechenden Werte, die Sie aus dem Formular abgerufen haben. 
+//Führen Sie dann den Befehl aus, um die Daten in die Tabelle einzufügen:
+$stmt = $db->prepare($sql);
+$stmt->bindParam(":vorname", $Vorname);
+$stmt->bindParam(":nachname", $Nachname);
+$stmt->bindParam(":email", $EMail);
+$stmt->bindParam(":benutzername", $Benutzername);
+$stmt->bindParam(":passwort", $Passwort);
+$stmt->execute();
 
+echo "Registrierung erfolgreich!"; //Überprüfung, ob erfolgreich   
+$db = null; // Datenbankverbindung schließen
