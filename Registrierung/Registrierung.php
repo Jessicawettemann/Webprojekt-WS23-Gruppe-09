f951acf301dacddb631243d4e2219631627933ac
Alina Kell <alina.kell41@gmail.com>
2023-11-03 15:44:18 +0100

    Create Registrierung.php

---
+++ Registrierung/Registrierung.php
@@ -0,0 +1,6 @@
+<?<php
+//Verbingsinformationen
+$hostname = "mars.iuk.hdm-stuttgart.de"; // Hostname Datenbank
+$username = "jw170"; // Datenbank-Benutzername
+$password = "Air8aing3r"; // Datenbank-Passwort
+$dbname = "u-jw170"; // Name  Datenbank
