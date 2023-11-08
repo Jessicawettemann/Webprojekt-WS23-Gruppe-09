1466ac1eee1b6fbb7296126ccb6beb7bbf04a90c
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-08 15:01:38 +0100

    Verbessert

--- Registrierung Neu/Registrierung_do.php
+++ Registrierung Neu/Registrierung_do.php
@@ -33,7 +33,7 @@ if ($_FILES["profilbild"]["name"] != null) {
     //Wenn ein Profilbild hochgeladen wurde, wird der Dateiname in der Variable $fileName gespeichert. Dann wird der Dateiname mit dem Punkt als Trennzeichen in ein Array aufgeteilt, um das Dateiformat zu erhalten.
     // Das Dateiformat wird in der Variable $fileType gespeichert, indem das letzte Element des Arrays verwendet wird.
 // Das Dateiformat wird in der Variable $fileType gespeichert, indem das letzte Element des Arrays verwendet wird.
-    if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "HEIC" or $fileType == "jpeg" or $fileType == "JPG") {
+    if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "jpeg" or $fileType == "JPG") {
         echo "";
     } else {
         die("<div class='fail'> Dein aktuelles Dateiformat wird nicht unterstützt. </div>");
@@ -57,10 +57,10 @@ if(($_POST["vorname"]) !=null and ($_POST["nachname"]) !=null and ($_POST["benut
     if($statement->execute(array(htmlspecialchars($_POST["vorname"]), htmlspecialchars($_POST["nachname"]), htmlspecialchars($_POST["benutzername"]), htmlspecialchars($_POST["email"]), htmlspecialchars($_FILES["profilbild"]["name"]), password_hash($_POST["passwort"].$p, PASSWORD_BCRYPT), ))){
         echo"<div class='fine'> Du wurdest erfolgreich registriertmm "."<br><br>";
     }else{
-        die("<div class='fail'> Diese Zugangsdaten sind bereits vergeben "."<br><br>". "<a href='Registrierung%20Formular.php'>Erneut versuchen</a> </div>");
+        die("<div class='fail'> Diese Zugangsdaten sind bereits vergeben "."<br><br>". "<a href='Registrierung_Formular.php'>Erneut versuchen</a> </div>");
     }
 }else{
-    echo"<div class='fail'> Alle Felder müssen ausgefüllt sein! "."<br><br>"."<a href='Registrierung%20Formular.php'>Erneut versuchen</a> </div>";
+    echo"<div class='fail'> Alle Felder müssen ausgefüllt sein! "."<br><br>"."<a href='Registrierung_Formular.php'>Erneut versuchen</a> </div>";
 } //Der Code bereitet dann eine SQL-Abfrage vor, um die Benutzerdaten in die Datenbanktabelle "Nutzer" einzufügen. Es werden verschiedene Felder überprüft, um sicherzustellen, dass sie nicht leer sind. Wenn alle Felder ausgefüllt sind, wird die Abfrage ausgeführt und eine Erfolgsmeldung angezeigt.
 // Andernfalls wird eine Fehlermeldung angezeigt, die darauf hinweist, dass alle Felder ausgefüllt sein müssen.
 ?>
