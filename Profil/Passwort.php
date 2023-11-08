809d50add6af5c3ccea57ee15fc7b1bc8004d663
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-08 15:23:52 +0100

    Do seite

---
+++ Profil/Passwort.php
@@ -0,0 +1,44 @@
+<?php
+
+include 'Datenbank Verbindung.php';
+
+session_start();
+if(!isset($_SESSION["Nutzer_ID"])){
+    echo("<div class='fail'> Bitte melde dich zunächst an! "."<br><br>". "<a href='../Login/login.php'>Hier geht's zum Login</a> </div>");
+}else{
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Passwort bearbeiten</title>
+
+</head>
+<body>
+<h1>Passwort bearbeiten</h1>
+
+<!-- Datensatz aus der Datenbank holen von angemeldetem Nutzer -->
+<?php
+$Nutzer = $_SESSION["Nutzer_ID"];
+$statement=$pdo->prepare("SELECT * FROM Nutzer WHERE ID = ?");
+if (!$statement->execute([$Nutzer])){
+    die("<div class='fail'>Datensatz nicht verfügbar</div>");
+}
+$row = $statement->fetch();
+$Nutzer_Id = $row["ID"];
+$passwort = $row["passwort"];
+?>
+
+<!-- Formular Profil Passwort bearbeiten-->
+<form class="rows" action = "passwort_do.php?Nutzer_Id=<?php echo $Nutzer_Id; ?>" method="post">
+    <label for="passwort_input"></label>
+    <input type="password" name="passwort" id="passwortinput" placeholder="Passwort"> <br>
+    <p><input type="submit" value="Änderung bestätigen"></p>
+</form>
+<?php
+}
+?>
+</body>
+</html>
