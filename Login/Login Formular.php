6bd53bee4e77d8db8ce80c186dec9dd3367e1c50
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-13 16:05:08 +0100

    Benutzername

--- Login/Login Formular.php
+++ Login/Login Formular.php
@@ -1,32 +1,37 @@
 <?php
+include "Datenbank Verbindung.php";
+
 session_start();
-include"Datenbank Verbindung.php";
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <title>Login Formular</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+
+    <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
+<form
+<div class="input_container"
+<form action="Login%20Do.php" method="post">
+    <h1> Login</h1>
+    <label class="label" for="benutzername"></label>
+    <input type="text" placeholder= "Benutzername" name="benutzername">
 
-        <div class="input_container"
-        <form action="Login%20Do.php" method="post">
+    <label class="label" for="passwort"></label>
+    <input type="password" placeholder="Passwort" name="passwort">
 
-            <label class="label" for="benutzername"></label>
-            <input type="text" placeholder= "Benutzername" name="benutzername">
+    <button type="submit">Einloggen</button>
+    <br><br>
+    <p>Noch keinen Account? <br> <a href="Registrierung_Formular.php">Hier registrieren</a>
+    </p>
+</form>
+</div>
+</form>
 
-            <label class="label" for="passwort"></label>
-            <input type="password" placeholder="Passwort" name="passwort">
-
-            <button type="submit">Einloggen</button>
-            <br><br>
-            <p>Noch keinen Account? <br> <a href="Registrierung_Formular.php">Hier registrieren</a>
-            </p>
-        </form>
-        </div>
-    </form>
 </body>
+
+</html>
+
