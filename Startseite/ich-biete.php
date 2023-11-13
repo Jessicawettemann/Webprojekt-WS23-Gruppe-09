477c16be8d3dd5c621e59aeb6117983009a6393e
Alina Kell <alina.kell41@gmail.com>
2023-11-13 20:10:43 +0100

    Create ich-biete.php

---
+++ Startseite/ich-biete.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+</head>
+
+<body>
+    <h1> Das ist unsere Ich biete-Seite </h1>
+
+</body>
+</html>
