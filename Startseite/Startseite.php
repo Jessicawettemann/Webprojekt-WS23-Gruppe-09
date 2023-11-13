2eca9942ee49c6c23523f49b5524c913c8a5486e
Alina Kell <alina.kell41@gmail.com>
2023-11-13 19:41:48 +0100

    Update Startseite.php

--- Startseite/Startseite.php
+++ Startseite/Startseite.php
@@ -14,26 +14,22 @@
     <div class="ich-suche"> <!-- Klasse ist ein gruppierendes Element ohne semantische Bedeutung 
 Durch die Klasse kann ein einziges Element innerhalb des div mit CSS angesprochen werden -->
         <h2>Ich suche</h2>
-        <p>Willkommen in Kategorie 1. Hier findest du interessante Inhalte.</p>
-        <a href="redirect.php?category=1">Zur Kategorie 1</a>
+        <a href="redirect.php?category=1">Weiterleitung ich-suche</a>
     </div>
 
     <div class="ich-biete">
         <h2>Ich biete</h2>
-        <p>Entdecke spannende Informationen in Kategorie 2.</p>
-        <a href="redirect.php?category=2">Zur Kategorie 2</a>
+        <a href="redirect.php?category=2">Weiterleitung ich-biete</a>
     </div>
 
     <div class="community">
         <h2>Community</h2>
-        <p>Hier gibt es interessante Beiträge in Kategorie 3.</p>
-        <a href="redirect.php?category=3">Zur Kategorie 3</a>
+        <a href="redirect.php?category=3">Weiterleitung community</a>
     </div>
 
     <div class="aktivitaeten">
         <h2>Aktivitäten</h2>
-        <p>Tauche ein in Kategorie 4 und entdecke aufregende Themen.</p>
-        <a href="redirect.php?category=4">Zur Kategorie 4</a>
+        <a href="redirect.php?category=4">Weiterleitung aktivitaeten</a>
     </div>
 
 </body>
