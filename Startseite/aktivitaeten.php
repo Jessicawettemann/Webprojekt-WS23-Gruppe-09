0937cd5e772c360c41cd48ca43bf6b5e3a5a8231
Alina Kell <alina.kell41@gmail.com>
2023-11-13 20:10:37 +0100

    Create aktivitaeten.php

---
+++ Startseite/aktivitaeten.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+</head>
+
+<body>
+    <h1> Das ist unsere Aktivitäten-Seite </h1>
+
+</body>
+</html>
