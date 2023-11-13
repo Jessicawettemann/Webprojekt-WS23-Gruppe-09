828286d7ffb8ea0cccbac0acea3eafb9e475ab67
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-13 15:17:43 +0100

    Benutzername

--- Login/Login Formular.php
+++ Login/Login Formular.php
@@ -12,11 +12,11 @@ session_start();
 <body>
     <h1>Login</h1>
     <form action="Login Do.php" method="post">
-        <label for="username">Benutzername:</label>
-        <input type="text" id="username" name="username" required><br>
+        <label for="benutzername">Benutzername:</label>
+        <input type="text" id="benutzername" name="benutzername" required><br>
 
-        <label for="password">Passwort:</label>
-        <input type="password" id="password" name="password" required><br>
+        <label for="passwort">Passwort:</label>
+        <input type="passwort" id="passwort" name="passwort" required><br>
 
         <button type="submit">Absenden</button>
     </form>
