7dc912e1a0989dd8581fae0406352c1ae85854c3
Alina Kell <alina.kell41@gmail.com>
2023-11-18 18:55:01 +0100

    Update aktivitaeten.php

--- Aktivitäten/aktivitaeten.php
+++ Aktivitäten/aktivitaeten.php
@@ -6,28 +6,85 @@ include "Header Sicherheit.php";
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Startseite</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminkalender</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
-
 <body>
-    <h1> Das ist unsere Aktivitäten-Seite </h1>
-    <h2> Kalender </h2>
-    
-    <!-- Kalenderbereich -->
-    <div id="calendar">
-        <!-- Hier wird der Kalender angezeigt -->
-    </div>
 
-    <!-- Formular zum Hinzufügen von Ereignissen -->
-    <form action="add_event.php" method="post">
-        <label for="event">Ereignis hinzufügen:</label>
-        <label for="event">Beschreibung hinzufügen:</label>
-        <input type="text" id="event" name="event" required>
-        <label for="date">Datum:</label>
-        <input type="date" id="date" name="date" required>
-        <button type="submit">Ereignis hinzufügen</button>
-    </form>
+<?php
+// Hier können Sie Termine festlegen oder aus einer Datenquelle abrufen
+$appointments = array(
+    "2023-11-01" => array("Meeting 1", "Appointment"),
+    "2023-11-05" => array("Coffee with friend", "Conference call"),
+    "2023-11-10" => array("Lunch meeting"),
+    // ... weitere Termine
+);
 
-</body>
-</html>
+echo '<div id="calendar">';
+
+// Wochentage
+$daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// Aktuelles Datum
+$currentDate = new DateTime();
+
+// Monat und Jahr des aktuellen Datums
+$currentMonth = $currentDate->format('n');
+$currentYear = $currentDate->format('Y');
+
+// Tage im aktuellen Monat
+$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+
+// Erster Tag des Monats
+$firstDayOfMonth = new DateTime("$currentYear-$currentMonth-01");
+$firstDayOfWeek = $firstDayOfMonth->format('N');
+
+// Kalenderkopf erstellen
+echo '<div class="day">Week</div>';
+foreach ($daysOfWeek as $day) {
+    echo '<div class="day">' . $day . '</div>';
+}
+
+// Leere Zellen für den Anfang des Monats hinzufügen
+for ($i = 1; $i < $firstDayOfWeek; $i++) {
+    echo '<div class="day"></div>';
+}
+
+// Tage des Monats hinzufügen
+for ($day = 1; $day <= $daysInMonth; $day++) {
+    $currentDateStr = "$currentYear-$currentMonth-" . sprintf('%02d', $day);
+    $appointmentsForDay = $appointments[$currentDateStr] ?? [];
+
+    echo '
+        <div class="day" data-date="' . $currentDateStr . '">
+            <div>' . $day . '</div>
+            <ul class="events">
+                ' . implode('', array_map(function ($appointment) {
+                    return '<li>' . $appointment . '</li>';
+                }, $appointmentsForDay)) . '
+            </ul>
+        </div>';
+}
+
+echo '</div>';
+
+
+//hier folgt JavaScript Code:
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Ereignislistener für Klick auf einen Tag hinzufügen
+    const days = document.querySelectorAll(".day");
+    days.forEach(day => {
+        day.addEventListener("click", handleDayClick);
+    });
+});
+
+function handleDayClick(event) {
+    const selectedDate = event.currentTarget.getAttribute("data-date");
+    alert(`Termine für ${selectedDate}`);
+    // Hier können Sie weitere Aktionen für den ausgewählten Tag implementieren
+}
+
+
+?>
