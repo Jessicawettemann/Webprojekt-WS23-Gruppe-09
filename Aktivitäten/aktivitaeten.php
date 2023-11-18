695cabcf209b47b745dd4ac33e69f9f2bd2d9502
Alina Kell <alina.kell41@gmail.com>
2023-11-18 18:57:25 +0100

    Update aktivitaeten.php

--- Aktivitäten/aktivitaeten.php
+++ Aktivitäten/aktivitaeten.php
@@ -12,79 +12,21 @@ include "Header Sicherheit.php";
 </head>
 <body>
 
-<?php
-// Hier können Sie Termine festlegen oder aus einer Datenquelle abrufen
-$appointments = array(
-    "2023-11-01" => array("Meeting 1", "Appointment"),
-    "2023-11-05" => array("Coffee with friend", "Conference call"),
-    "2023-11-10" => array("Lunch meeting"),
-    // ... weitere Termine
-);
+    <h1>Kalender</h1>
+    
+    <!-- Kalenderbereich -->
+    <div id="calendar">
+        <!-- Hier wird der Kalender angezeigt -->
+    </div>
 
-echo '<div id="calendar">';
-
-// Wochentage
-$daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-// Aktuelles Datum
-$currentDate = new DateTime();
-
-// Monat und Jahr des aktuellen Datums
-$currentMonth = $currentDate->format('n');
-$currentYear = $currentDate->format('Y');
-
-// Tage im aktuellen Monat
-$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
-
-// Erster Tag des Monats
-$firstDayOfMonth = new DateTime("$currentYear-$currentMonth-01");
-$firstDayOfWeek = $firstDayOfMonth->format('N');
-
-// Kalenderkopf erstellen
-echo '<div class="day">Week</div>';
-foreach ($daysOfWeek as $day) {
-    echo '<div class="day">' . $day . '</div>';
-}
-
-// Leere Zellen für den Anfang des Monats hinzufügen
-for ($i = 1; $i < $firstDayOfWeek; $i++) {
-    echo '<div class="day"></div>';
-}
-
-// Tage des Monats hinzufügen
-for ($day = 1; $day <= $daysInMonth; $day++) {
-    $currentDateStr = "$currentYear-$currentMonth-" . sprintf('%02d', $day);
-    $appointmentsForDay = $appointments[$currentDateStr] ?? [];
-
-    echo '
-        <div class="day" data-date="' . $currentDateStr . '">
-            <div>' . $day . '</div>
-            <ul class="events">
-                ' . implode('', array_map(function ($appointment) {
-                    return '<li>' . $appointment . '</li>';
-                }, $appointmentsForDay)) . '
-            </ul>
-        </div>';
-}
-
-echo '</div>';
-
-
-//hier folgt JavaScript Code:
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Ereignislistener für Klick auf einen Tag hinzufügen
-    const days = document.querySelectorAll(".day");
-    days.forEach(day => {
-        day.addEventListener("click", handleDayClick);
-    });
-});
-
-function handleDayClick(event) {
-    const selectedDate = event.currentTarget.getAttribute("data-date");
-    alert(`Termine für ${selectedDate}`);
-    // Hier können Sie weitere Aktionen für den ausgewählten Tag implementieren
-}
-
-
-?>
+    <!-- Formular zum Hinzufügen von Ereignissen -->
+    <form action="add_event.php" method="post">
+        <label for="event">Ereignis hinzufügen:</label>
+        <label for="event">Beschreibung hinzufügen:</label>
+        <input type="text" id="event" name="event" required>
+        <label for="date">Datum:</label>
+        <input type="date" id="date" name="date" required>
+        <button type="submit">Ereignis hinzufügen</button>
+    </form>
+</body>
+</html>
