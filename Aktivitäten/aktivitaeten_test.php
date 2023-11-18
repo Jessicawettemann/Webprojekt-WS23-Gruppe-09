3d9eb9bb5c631287d5ab1cf957203c1d6ef83342
Alina Kell <alina.kell41@gmail.com>
2023-11-18 18:36:51 +0100

    Update aktivitaeten_test.php

--- Aktivitäten/aktivitaeten_test.php
+++ Aktivitäten/aktivitaeten_test.php
@@ -8,12 +8,6 @@ include "Header Sicherheit.php";
     <meta charset="UTF-8">
     <title>Startseite</title>
 
-</head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Terminkalender</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
 <body>
     <div id="calendar"></div>
 
