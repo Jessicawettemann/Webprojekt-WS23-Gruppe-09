1d637f9d3c0ed9cbe9ce2cb85397b8e083aecf10
Alina Kell <alina.kell41@gmail.com>
2023-11-18 18:55:05 +0100

    Update aktivitaeten_do.php

---
+++ Aktivitäten/aktivitaeten_do.php
@@ -0,0 +1,59 @@
+
+
+<?php
+// Hier können Sie Termine festlegen oder aus einer Datenquelle abrufen
+$appointments = array(
+    "2023-11-01" => array("Meeting 1", "Appointment"),
+    "2023-11-05" => array("Coffee with friend", "Conference call"),
+    "2023-11-10" => array("Lunch meeting"),
+    // ... weitere Termine
+);
+
+echo '<div id="calendar">';
+
+// Wochentage
+$daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// Aktuelles Datum
+$currentDate = new DateTime();
+
+// Monat und Jahr des aktuellen Datums
+$currentMonth = $currentDate->format('n');
+$currentYear = $currentDate->format('Y');
+
+// Tage im aktuellen Monat
+$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+
+// Erster Tag des Monats
+$firstDayOfMonth = new DateTime("$currentYear-$currentMonth-01");
+$firstDayOfWeek = $firstDayOfMonth->format('N');
+
+// Kalenderkopf erstellen
+echo '<div class="day">Week</div>';
+foreach ($daysOfWeek as $day) {
+    echo '<div class="day">' . $day . '</div>';
+}
+
+// Leere Zellen für den Anfang des Monats hinzufügen
+for ($i = 1; $i < $firstDayOfWeek; $i++) {
+    echo '<div class="day"></div>';
+}
+
+// Tage des Monats hinzufügen
+for ($day = 1; $day <= $daysInMonth; $day++) {
+    $currentDateStr = "$currentYear-$currentMonth-" . sprintf('%02d', $day);
+    $appointmentsForDay = $appointments[$currentDateStr] ?? [];
+
+    echo '
+        <div class="day" data-date="' . $currentDateStr . '">
+            <div>' . $day . '</div>
+            <ul class="events">
+                ' . implode('', array_map(function ($appointment) {
+                    return '<li>' . $appointment . '</li>';
+                }, $appointmentsForDay)) . '
+            </ul>
+        </div>';
+}
+
+echo '</div>';
+?>
