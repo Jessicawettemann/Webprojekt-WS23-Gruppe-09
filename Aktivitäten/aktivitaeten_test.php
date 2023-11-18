08f491e6c9c4ddddcb3d0c6d976b5517d6a21adc
Alina Kell <alina.kell41@gmail.com>
2023-11-18 18:35:25 +0100

    Create aktivitaeten_test.php

---
+++ Aktivitäten/aktivitaeten_test.php
@@ -0,0 +1,22 @@
+<?php
+include "Header Sicherheit.php";
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+
+</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminkalender</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="calendar"></div>
+
+    <script src="calendar.js"></script>
+</body>
+</html>
