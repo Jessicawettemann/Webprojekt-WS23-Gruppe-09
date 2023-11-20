7619bdbe3d92d956c9db796f6cd09aedf232583f
Alina Kell <alina.kell41@gmail.com>
2023-11-20 17:39:41 +0100

    Update aktivitaeten_do.php

--- Aktivitäten/aktivitaeten_do.php
+++ Aktivitäten/aktivitaeten_do.php
@@ -1,59 +1,37 @@
-
-
 <?php
-// Hier können Sie Termine festlegen oder aus einer Datenquelle abrufen
-$appointments = array(
-    "2023-11-01" => array("Meeting 1", "Appointment"),
-    "2023-11-05" => array("Coffee with friend", "Conference call"),
-    "2023-11-10" => array("Lunch meeting"),
-    // ... weitere Termine
-);
-
-echo '<div id="calendar">';
-
-// Wochentage
-$daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-// Aktuelles Datum
-$currentDate = new DateTime();
-
-// Monat und Jahr des aktuellen Datums
-$currentMonth = $currentDate->format('n');
-$currentYear = $currentDate->format('Y');
-
-// Tage im aktuellen Monat
-$daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
-
-// Erster Tag des Monats
-$firstDayOfMonth = new DateTime("$currentYear-$currentMonth-01");
-$firstDayOfWeek = $firstDayOfMonth->format('N');
-
-// Kalenderkopf erstellen
-echo '<div class="day">Week</div>';
-foreach ($daysOfWeek as $day) {
-    echo '<div class="day">' . $day . '</div>';
-}
-
-// Leere Zellen für den Anfang des Monats hinzufügen
-for ($i = 1; $i < $firstDayOfWeek; $i++) {
-    echo '<div class="day"></div>';
-}
-
-// Tage des Monats hinzufügen
-for ($day = 1; $day <= $daysInMonth; $day++) {
-    $currentDateStr = "$currentYear-$currentMonth-" . sprintf('%02d', $day);
-    $appointmentsForDay = $appointments[$currentDateStr] ?? [];
-
-    echo '
-        <div class="day" data-date="' . $currentDateStr . '">
-            <div>' . $day . '</div>
-            <ul class="events">
-                ' . implode('', array_map(function ($appointment) {
-                    return '<li>' . $appointment . '</li>';
-                }, $appointmentsForDay)) . '
-            </ul>
-        </div>';
-}
-
-echo '</div>';
+include 'Datenbank Verbindung.php';
+include "Header Sicherheit.php";
+session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalender</title>
+
+</head>
+
+<body>
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $beschreibung = $_POST['beschreibung'];
+    $datum = $_POST['datum'];
+    $ort = $_POST['ort'];
+
+    // SQL-Query zum Hinzufügen des Ereignisses
+    $sql = "INSERT INTO Aktivitäten (name, beschreibung, datum, ort) VALUES ('$name', '$beschreibung', '$datum', '$ort')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Ereignis wurde erfolgreich hinzugefügt";
+    } else {
+        echo "Fehler beim Hinzufügen des Ereignisses: " . $conn->error;
+    }
+}
+
+// Datenbankverbindung schließen
+$conn->close();
+?>
+
