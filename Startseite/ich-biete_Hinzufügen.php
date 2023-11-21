b5ac26e61010faabf19315710a4725db876cb66f
fb106 <147589337+fb106@users.noreply.github.com>
2023-11-21 12:28:57 +0100

    Create ich-biete_Hinzufügen.php

---
+++ Startseite/ich-biete_Hinzufügen.php
@@ -0,0 +1,21 @@
+<?php
+include "Datenbank Verbindung.php";
+include "Header Sicherheit.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+    <title>Angebotsübersicht</title>
+</head>
+
+<body>
+    <h1> Das ist unsere Ich biete-Seite </h1>
+</body>
+</html>
+
+</body>
+</html>
