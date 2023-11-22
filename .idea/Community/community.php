c55a6663c1610fcb6c0f93f457408aa06a2672ee
Alina Kell <alina.kell41@gmail.com>
2023-11-22 16:14:49 +0100

    Create community.php

---
+++ .idea/Community/community.php
@@ -0,0 +1,15 @@
+<?php
+include "Header Sicherheit.php";
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminkalender</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+
