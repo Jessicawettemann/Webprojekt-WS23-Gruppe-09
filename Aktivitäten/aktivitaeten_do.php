2b9cc4d843a8b4351503b3903f76f82badbf2d7d
Alina Kell <alina.kell41@gmail.com>
2023-11-23 10:28:21 +0100

    Update aktivitaeten_do.php

--- Aktivitäten/aktivitaeten_do.php
+++ Aktivitäten/aktivitaeten_do.php
@@ -1,79 +1,39 @@
 <?php
-include 'Datenbank Verbindung.php'; 
+include 'Datenbank Verbindung.php';
 include "Header Sicherheit.php";
 session_start();
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <title>Kalender</title>
+   <meta charset="UTF-8">
+   <title>Kalender</title>
+
 
 </head>
+
 
 <body>
 <?php
 
 
+// Binde die Werte der Felder an Parametermarkierungen
+$statement = $pdo->prepare("INSERT INTO Aktivitäten (thema, beschreibung, datum, ort) VALUES (?,?,?,?)");
 
-// Verarbeite die Ereignisinformationen
-$einfuegen = $db->prepare(
-    "INSERT INTO Aktivitäten (thema, beschreibung, datum, ort) 
-    VALUES (?, ?, ?, ?, NOW())");
-
-$einfuegen->bind_param('ssss', $thema, $beschreibung, $datum, $ort);
+$stmt->bind_param("ssss", $thema, $beschreibung, $datum, $ort);
 
 
-    if ($einfuegen->execute()) {
-        header('Location: index.php');
-        die('erfolgreich');
-    }
-
-?>
-
-
-// Daten ausgeben lassen
+// Setze die Werte der Parameter und führe den Anweisungsvorgang aus
+$beschreibung = $_POST['beschreibung'];
+$thema = $_POST['thema'];
+$datum = $_POST['datum'];
+$ort = $_POST['ort'];
+$stmt->execute();
 
 
-// Überprüfe, ob die Verbindung erfolgreich war
-if ($conn->connect_error) {
-    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-}
+echo "Neuer Eintrag wurde erfolgreich erstellt!";
 
-// Führe eine SELECT-Anweisung aus, um alle Datensätze aus der Tabelle Aktivitäten abzurufen
-$sql = "SELECT id, beschreibung, datum, ort FROM Aktivitäten ORDER BY datum";
-$result = $conn->query($sql);
 
-// Starte eine HTML-Tabelle, um die Daten anzuzeigen
-echo "<table border='1'>
-<tr>
-<th>ID</th>
-<th>Beschreibung</th>
-<th>Datum</th>
-<th>Ort</th>
-</tr>";
-
-// Gehe durch alle Zeilen in den Abfrageergebnissen und füge sie in die Tabelle ein
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["id"]. "</td>";
-        echo "<td>" . $row["beschreibung"]. "</td>";
-        echo "<td>" . $row["datum"]. "</td>";
-        echo "<td>" . $row["ort"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "<tr><td colspan='5'>Keine Aktivitäten gefunden</td></tr>";
-}
-
-// Schließe die HTML-Tabelle ab
-echo "</table>";
-
-// Schließe die Verbindung zur Datenbank
-$conn->close();
-
-?>
-
+$stmt->close();
