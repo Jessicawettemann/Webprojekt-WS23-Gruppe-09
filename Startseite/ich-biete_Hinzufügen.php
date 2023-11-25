96fff802b7ec6603d8cdcf54fdf6b831433ada5d
fb106 <147589337+fb106@users.noreply.github.com>
2023-11-25 17:34:20 +0100

    Update ich-biete_Hinzufügen.php

--- Startseite/ich-biete_Hinzufügen.php
+++ Startseite/ich-biete_Hinzufügen.php
@@ -11,11 +11,41 @@ session_start();
     <title>Startseite</title>
     <title>Angebotsübersicht</title>
 </head>
-
 <body>
-    <h1> Das ist unsere Ich biete-Seite </h1>
-</body>
-</html>
+    
+<?php
+if(!isset($_SESSION["User"])){
+    echo("<div class='fail'> Diese Funktion steht nur den Nutzern zu Verfügung! "."<br><br>". "<a href='../Login Formular.php'>Hier geht's zum Login</a> </div>");
+}else{
+    ?>
+
+    <form action="ich-biete_Hinzufügen_do.php" method="post" enctype="multipart/form-data">
+        <label for="thema">Beschreibung:</label>
+        <input type="text" id="thema" name="thema" required>
+
+        <label for="foto">Foto hinzufügen:</label>
+        <input type="file" id="Foto" placeholder="Foto">
+
+        <label for="beschreibung">Zustand::</label>
+        <input type="text" id="Zustand" name="Zustand" required>
+
+        <label for="datum">Datum:</label>
+        <input type="date" id="Datum" name="Datum" required>
+
+        <label for="preis">Preis:</label>
+        <input type="numbers" id="Preis" name="Preis" required>
+
+        <label for="ort">Ort:</label>
+        <input type="text" id="Ort" name="Ort" required>
+
+        <button type="submit">Angebot hinzufügen</button>
+        
+        
+    </form>
+
+<?php
+}
+?>
 
 </body>
 </html>
