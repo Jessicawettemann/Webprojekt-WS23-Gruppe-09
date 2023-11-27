ae4c15b78de3c1a42330340e074648fa69f88cb5
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-27 15:21:03 +0100

    Styling

--- Registrierung Neu/Registrierung_do.php
+++ Registrierung Neu/Registrierung_do.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include"Datenbank Verbindung.php";
-include "Header Sicherheit.php";
+
 
 ?>
 
@@ -16,55 +16,42 @@ include "Header Sicherheit.php";
 <body>
 
 <?php
+
 if(!isset($_POST["benutzername"]) or !isset($_POST["passwort"])){
     die("<div class='fail'> Formularfehler </div>");
-} //Der Code überprüft, ob die POST-Variablen "benutzername" und "passwort" gesetzt sind.
-// Wenn nicht, wird eine Fehlermeldung angezeigt und der Code wird beendet.
-echo "1";
+}
+
 if ($_FILES["profilbild"]["name"] != null) {
-    echo "2";
     $fileName = $_FILES["profilbild"]["name"];
     $fileSplit = explode(".", $fileName);
     $fileType = $fileSplit[sizeof($fileSplit) - 1];
-//In diesem Codeabschnitt wird überprüft, ob ein Profilbild hochgeladen wurde.
-// Dazu wird überprüft, ob der Wert des "name" Feldes des "profilbild" Arrays in $_FILES nicht null ist.
-//Wenn ein Profilbild hochgeladen wurde, wird der Dateiname in der Variable $fileName gespeichert.
-// Dann wird der Dateiname mit dem Punkt als Trennzeichen in ein Array aufgeteilt, um das Dateiformat zu erhalten.
-    //In diesem Codeabschnitt wird überprüft, ob ein Profilbild hochgeladen wurde. Dazu wird überprüft, ob der Wert des "name" Feldes des "profilbild" Arrays in $_FILES nicht null ist.
-    //
-    //Wenn ein Profilbild hochgeladen wurde, wird der Dateiname in der Variable $fileName gespeichert. Dann wird der Dateiname mit dem Punkt als Trennzeichen in ein Array aufgeteilt, um das Dateiformat zu erhalten.
-    // Das Dateiformat wird in der Variable $fileType gespeichert, indem das letzte Element des Arrays verwendet wird.
-// Das Dateiformat wird in der Variable $fileType gespeichert, indem das letzte Element des Arrays verwendet wird.
-    if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "jpeg" or $fileType == "JPG") {
+
+    if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "HEIC" or $fileType == "jpeg" or $fileType == "JPG") {
         echo "";
     } else {
-        die("<div class='fail'> Dein aktuelles Dateiformat wird nicht unterstützt. </div>");
-
+        die("<div class='fail'> Dieses Dateiformat wird nicht unterstützt. </div>");
+    }
+    if ($_FILES["profilbild"]["size"] > 80000000) {
+        die("<div class='fail'> Diese Datei ist leider zu groß </div>");
     }
     if (!move_uploaded_file($_FILES["profilbild"]["tmp_name"], "/home/jw170/public_html/Bilder/" .$_FILES["profilbild"]["name"])) {
         echo "<div class='fail'> Datenbankfehler </div>";
-    }else{
-        echo "ok";
-    } //Wenn ein Profilbild hochgeladen wird, überprüft der Code das Dateiformat und verschiebt die Datei in das Verzeichnis
-    // "/home/jw170/public_html/Bilder/". Wenn das Profilbild zu groß ist, wird eine Fehlermeldung angezeigt.
-}
-    if ($_FILES["profilbild"]["size"] > 80000000) {
-    die("<div class='fail'> Deine aktuellle Datei ist leider zu groß </div>");
+    }
 }
 $statement = $pdo->prepare("INSERT INTO Nutzer (vorname, nachname, benutzername, email, profilbild, passwort) VALUES (?,?,?,?,?,?)");
 $p = "hjfew3545r8c0szhwgfsdafghjgfdhj";
 
-// Felder sollen nicht freigelassen werden wenn doch Fehlermeldung:
+// Felder sollen nicht freigelassen werden:
 if(($_POST["vorname"]) !=null and ($_POST["nachname"]) !=null and ($_POST["benutzername"]) !=null and ($_POST["email"]) !=null and ($_POST["passwort"]) !=null){
     if($statement->execute(array(htmlspecialchars($_POST["vorname"]), htmlspecialchars($_POST["nachname"]), htmlspecialchars($_POST["benutzername"]), htmlspecialchars($_POST["email"]), htmlspecialchars($_FILES["profilbild"]["name"]), password_hash($_POST["passwort"].$p, PASSWORD_BCRYPT), ))){
-        echo"<div class='fine'> Du wurdest erfolgreich registriertmm "."<br><br>";
+        echo"<div class='fine'> Du wurdest erfolgreich registriert "."<br><br>"."<a href='Startseite.php'>Hier kommst du zur Startseite</a> </div>";
     }else{
         die("<div class='fail'> Diese Zugangsdaten sind bereits vergeben "."<br><br>". "<a href='Registrierung_Formular.php'>Erneut versuchen</a> </div>");
     }
 }else{
     echo"<div class='fail'> Alle Felder müssen ausgefüllt sein! "."<br><br>"."<a href='Registrierung_Formular.php'>Erneut versuchen</a> </div>";
-} //Der Code bereitet dann eine SQL-Abfrage vor, um die Benutzerdaten in die Datenbanktabelle "Nutzer" einzufügen. Es werden verschiedene Felder überprüft, um sicherzustellen, dass sie nicht leer sind. Wenn alle Felder ausgefüllt sind, wird die Abfrage ausgeführt und eine Erfolgsmeldung angezeigt.
-// Andernfalls wird eine Fehlermeldung angezeigt, die darauf hinweist, dass alle Felder ausgefüllt sein müssen.
+}
+
 ?>
 
 </body>
