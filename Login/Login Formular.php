2f7cad83ca76165fd7a435ea701f2ad1cc1e8ef7
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-11-27 15:56:34 +0100

    Styling

--- Login/Login Formular.php
+++ Login/Login Formular.php
@@ -15,21 +15,27 @@ session_start();
 <img src="https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/Teal Green Simple Illustrated Social Media Policy Presentation.png">
 <body>
 <form
-<div class="input_container"
+  <div class="center"
+<div class="input"
+<h1> Login</h1>
 <form action="Login%20Do.php" method="post">
     <h1> Login</h1>
     <label class="label" for="benutzername"></label>
     <input type="text" placeholder= "Benutzername" name="benutzername">
-
+    <span> </span>
     <label class="label" for="passwort"></label>
     <input type="password" placeholder="Passwort" name="passwort">
+    <span> </span>
 
     <button type="submit">Einloggen</button>
     <br><br>
     <p>Noch keinen Account? <br> <a href="Registrierung_Formular.php">Hier registrieren</a>
+
     </p>
+    </div>
 </form>
 </div>
+
 </form>
 
 </body>
