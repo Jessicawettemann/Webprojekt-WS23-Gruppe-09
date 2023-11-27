96872e985b928ad753e53f1078d78cb57bc5c945
fb106 <147589337+fb106@users.noreply.github.com>
2023-11-27 20:20:03 +0100

    Create ich-biete_Löschen_do.php

---
+++ Startseite/ich-biete_Löschen_do.php
@@ -0,0 +1,29 @@
+<?php
+include "Datenbank Verbindung.php";
+include "Header Sicherheit.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Angebote löschen</title>
+</head>
+<body>
+<?php
+if(!isset($_SESSION["Nutzer_ID"])) { #prüft, ob User eingeloggt ist
+    die("<div class='fail'>Du musst eingeloggt sein, um deine Angebote löschen zu können!". "<br><br>" . "<a href='../Login Formular.php'>Hier geht's zum Login</a> </div>");
+}
+if(isset($_GET["Ich_biete_ID"])){
+    $statement=$pdo->prepare("DELETE FROM Ich_biete WHERE ID=?");
+    if($statement->execute(array($_GET["ID"]))){
+        echo "<div class='fine'>Angebot wurde erfolgreich gelöscht</div>";
+    }else{
+        die ("<div class='fail'>Datenbank-Fehler</div>");
+    }
+}
+?>
+<a class="back" href="ich-biete_Übersicht.php"> zu den Angeboten </a>
+</body>
+</html>
