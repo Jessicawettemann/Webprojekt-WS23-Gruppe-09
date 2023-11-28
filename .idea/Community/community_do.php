22685a2d475a00d303c76785bebe788001f21e0c
Alina Kell <alina.kell41@gmail.com>
2023-11-28 18:31:58 +0100

    Update community_do.php

---
+++ .idea/Community/community_do.php
@@ -0,0 +1,42 @@
+<?php
+
+include "Datenbank Verbindung.php";
+include "Header Sicherheit.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+
+    <title>Kalender</title>
+</head>
+<body>
+
+<?php
+
+
+//Beitrag eintragen
+
+
+$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
+
+// Feld sollen nicht freigelassen werden:
+
+if(($_POST["beitrag"])){
+    
+
+
+        if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
+            echo "<div class='fine'> Beitrag gespeichert </div>". "<br><br>" . "<a href='community.php'>Zur Community</a> </div>";
+        } else {
+            die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+        }
+    }
+
+
+?>
+</body>
+</html>
