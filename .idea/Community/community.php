b8f25528a89a320b254781de1fb97475f7c3db7d
Alina Kell <alina.kell41@gmail.com>
2023-11-28 18:31:56 +0100

    Update community.php

--- .idea/Community/community.php
+++ .idea/Community/community.php
@@ -15,8 +15,8 @@ include "Header Sicherheit.php";
 
 <h1>Forum</h1>
 
-<form id="post-form">
-    <textarea id="post-content" placeholder="Gib hier deine Nachricht ein..."></textarea>
+<form id="post-form" action="community_do.php">
+    <textarea id="beitrag" placeholder="Gib hier deine Nachricht ein..."></textarea>
     <button type="submit">Senden</button>
 </form>
 
@@ -24,18 +24,7 @@ include "Header Sicherheit.php";
     <!-- Nachrichten werden hier angezeigt. -->
 </div>
 
-<br><br>
-
- <h1>Feed</h1>
- <form id="post-form">
-    <label for="post-input">Neuer Post:</label>
-    <input type="text" id="post-input" name="post-input">
-    <button type="submit">Posten</button>
- </form>
- <div id="posts-container"></div>
- <script src="script.js"></script>
 
 </body>
-
 </html>
 
