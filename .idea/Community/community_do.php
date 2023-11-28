4f66b8e0fd3719e563017b8098fcf876bda1d456
Alina Kell <alina.kell41@gmail.com>
2023-11-28 19:41:41 +0100

    Update community_do.php

--- .idea/Community/community_do.php
+++ .idea/Community/community_do.php
@@ -1,5 +1,4 @@
 <?php
-
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
@@ -8,33 +7,29 @@ session_start();
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width">
 
-    <title>Forum</title>
+
+   <title>Kalender</title>
 </head>
 <body>
 
 <?php
 
-// Formulardaten auslesen
-$beitrag = $_POST['beitrag'];
+//Beitrag eintragen
+$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
 
-// Formulardaten prüfen
-if (empty($beitrag)) {
-    // Wenn der Beitrag leer ist, Fehler auslösen
-    http_response_code(400);
-    echo "Der Beitrag darf nicht leer sein.";
-    exit();
-}
+// Feld sollen nicht freigelassen werden:
+if(($_POST["thema"]) !=null){
 
-// Daten in die Datenbank einfügen
-$stmt = $conn->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
-$stmt->bind_param("s", $beitrag);
-$stmt->execute();
-
-// Nach erfolgreicher Ausführung auf die Startseite weiterleiten
-header("Location: community.php");
-exit();
+       if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
+           echo "<div class='fine'> Beitrag gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
+       } else {
+           die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+       }
+   }
 
 ?>
+</body>
+</html>
