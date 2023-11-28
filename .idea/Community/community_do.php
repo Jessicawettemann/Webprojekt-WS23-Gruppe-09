dbb6b3aae9b3c795fcdecaad1272074949845c4e
Alina Kell <alina.kell41@gmail.com>
2023-11-28 19:20:04 +0100

    Update community_do.php

--- .idea/Community/community_do.php
+++ .idea/Community/community_do.php
@@ -11,24 +11,30 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
 
-    <title>Community Forum</title>
+    <title>Kalender</title>
 </head>
 <body>
 
 <?php
 
-// Beitrag eintragen
 
-if(isset($_POST["beitrag"]) && !empty($_POST["beitrag"])){
+//Beitrag eintragen
 
-    $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
+$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
 
-    if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
-        echo "<div class='fine'> Beitrag gespeichert </div>". "<br><br>" . "<a href='community.php'>Zur Community</a> </div>";
+// Feld sollen nicht freigelassen werden:
+
+if(($_POST["thema"]) !=null){
+
+        if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
+            echo "<div class='fine'> Beitrag gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
+        } else {
+            die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+        }
     } else {
-        die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+        die("<div class='fail'> Fehlgeschlagen: Das eingegebene Datum darf nicht in der Vergangenheit liegen. <br><br>" . "<a href='aktivitaeten.php'>Erneut versuchen</a> </div>");
     }
-}
+
 
 ?>
 </body>
