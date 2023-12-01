66d0ccee4a5afb1cccdf520e8c435be37bd082b0
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-01 13:31:56 +0100

    Styling

--- Profil/Profil_do.php
+++ Profil/Profil_do.php
@@ -37,14 +37,14 @@ if(!isset($_SESSION["Nutzer_ID"])){
     $profilbild = $row["profilbild"];
     echo "<div class='rows'>";
     echo "Benutzername:" ." " . $row ["benutzername"]."<a class='edit' href= 'benutzername.php'>" . " bearbeiten ". "</a>";
-    echo "<br>";
+    echo "<br><br>";
     echo "E-Mail:"." ". $row ["email"]. "<a class='edit' href= 'email.php'>" . " bearbeiten". "</a>";
-    echo "<br>";
+    echo "<br><br>";
     echo "<a href= 'Passwort.php'>". "Passwort ändern". "</a> <br>" ;
-    echo "<br>";
+    echo "<br><br>";
     echo "<img src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $row["profilbild"] . "'height='100px'> <br>".
         "<a href= 'Profilbild.php'>" . "aktuelles Profilbild ändern";
-    echo "<br>";
+    echo "<br><br>";
     echo "</div>";
 }
 ?>
