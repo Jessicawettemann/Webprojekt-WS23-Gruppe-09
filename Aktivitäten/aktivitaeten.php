b46e723178a6fffd6a3594c9e1684d7ce8991014
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-02 13:10:02 +0100

    Styling

--- Aktivitäten/aktivitaeten.php
+++ Aktivitäten/aktivitaeten.php
@@ -9,7 +9,7 @@ include "Header Sicherheit.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Terminkalender</title>
-    <link rel="stylesheet" type="text/css" href="Formular 1.css">
+    <link rel="stylesheet" type="text/css" href="Formulare_1.css">
 </head>
 <body>
 
