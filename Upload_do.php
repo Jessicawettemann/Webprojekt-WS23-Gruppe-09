88da311120a04195c1aca4ecd013532a8b2c89f5
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-11 13:32:17 +0100

    Styling

--- Upload_do.php
+++ Upload_do.php
@@ -34,27 +34,23 @@ if ($fileSize > 80000000) {
     die("<div class='fail'> Diese Datei ist leider zu groß </div>");
 }
 
-
-
-     if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["foto"]["name"])) {
-        echo "<div class='fail'> Datenbankfehler 1 </div>";
-
+if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["foto"]["name"])) {
+    echo "<div class='fail'> Datenbankfehler 1 </div>";
 }
+
 if (!isset($_POST["zustand"])) {
     die("<div class='fail'> Zustand eintrag falsch </div>");
 }
+
 if (!isset($_POST["preis"]) ) {
     die("<div class='fail'> Preis eintrag falsch </div>");
 }
+
 if (!isset($_POST["ort"])) {
     die("<div class='fail'> Ort eintrag falsch </div>");
 }
 
-
-
-
-
-if ((!empty($_POST["beschreibung"])) and (!empty($_POST["foto"])) and (!empty($_FILES["zustand"])) and (!empty($_FILES["preis"])) and (!empty($_FILES["ort"])))  {
+if ((!empty($_POST["beschreibung"])) and (!empty($_POST["foto"])) and (!empty($_FILES["zustand"])) and (!empty($_FILES["preis"])) and (!empty($_FILES["ort"]))) {
 
     $statement = $pdo->prepare("INSERT INTO Upload (beschreibung, foto, zustand, preis, ort) VALUES (?, ?, ?, ?, ?)");
     if ($statement->execute(array(htmlspecialchars($_POST["beschreibung"]), htmlspecialchars($_FILES["foto"]["name"]), htmlspecialchars($_FILES["zustand"]), htmlspecialchars($_FILES["preis"]), htmlspecialchars($_FILES["ort"])))) {
@@ -75,4 +71,3 @@ if ((!empty($_POST["beschreibung"])) and (!empty($_POST["foto"])) and (!empty($_
 
 </body>
 </html>
-
