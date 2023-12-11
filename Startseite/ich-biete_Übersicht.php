d1cb842c6362c5342d1b8fb0c9673d4d8277f200
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-11 16:26:48 +0100

    Styling

--- Startseite/ich-biete_Übersicht.php
+++ Startseite/ich-biete_Übersicht.php
@@ -1,6 +1,6 @@
 <?php
-include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
+include "Datenbank Verbindung.php";
 session_start();
 ?>
 
@@ -9,54 +9,41 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Angebotsübersicht</title>
-
+    <link rel="stylesheet" href="Übersicht.css">
 </head>
 
 <body>
-    <h1> Das ist unsere Ich biete-Seite </h1>
-    <a href="Upload.php"> <h3>Hochladen</h3> </a>
+<h1> Das ist unsere Ich biete-Seite </h1>
+<a href="Upload.php"> <h3>Hochladen</h3> </a>
 
+<?php
 
+$statement=$pdo->prepare("SELECT * FROM Upload");
+if ($statement->execute()){
+    while($row=$statement->fetch()) {
+        $chosenSong = $row["ID"];
+        echo "<div class='card'>";
+        echo "<h4>".$row["beschreibung"]."</h4>";
 
-
-
-
-    <?php
-
-    # Einbindung von Hinzufüge-Button (Submit); hinterlegter Link href= playlistauswahl.php - in URL ausgewählter Song_ID in Variable speichern und mitschicken - $_GET("id")
-
-    $statement=$pdo->prepare("SELECT * FROM Upload");
-    if ($statement->execute()){
-        while($row=$statement->fetch()) {
-            $chosenSong = $row["ID"];
-            echo  "<h4>".$row["beschreibung"]."</h4>" ."<body class='small'>"." </body>";
-
-
-            if (!empty($row["foto"])) {
-                echo"<div class='image'>";
-                echo "<br> <img src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $row["foto"] . "'>" . "<br>";
-                echo "</div>";
-            } else{
-                echo "<div class='small'>keine Bilddatei vorhanden</div>";
-            }
-
-            echo  "<h4>".$row["zustand"]."</h4>" ."<body class='small'>"." </body>";
-
-            echo  "<h4>".$row["preis"]."</h4>" ."<body class='small'>"." </body>";
-
-            echo  "<h4>".$row["ort"]."</h4>" ."<body class='small'>"." </body>";
-
-
+        if (!empty($row["foto"])) {
+            echo"<div class='image'>";
+            echo "<img src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $row["foto"] . "'>";
+            echo "</div>";
+        } else{
+            echo "<div class='small'>keine Bilddatei vorhanden</div>";
         }
-    }else{
-        echo "<div class='fail'>Fehlermeldung!</div>";
-        echo $statement->errorInfo()[2];
-        die();
+
+        echo "<h4>".$row["zustand"]."</h4>";
+        echo "<h4>".$row["preis"]."</h4>";
+        echo "<h4>".$row["ort"]."</h4>";
+        echo "</div>";
     }
-    ?>
-
-
-
+}else{
+    echo "<div class='fail'>Fehlermeldung!</div>";
+    echo $statement->errorInfo()[2];
+    die();
+}
+?>
 
 </body>
 </html>
