857894cb7355840c74d47efa85931877113ce501
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-11 13:41:52 +0100

    Styling

--- Upload_do.php
+++ Upload_do.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width">
 </head>
 <?php
-if (!isset($_POST["beschreibung"])  or !isset($_POST["length"])) {
+if (!isset($_POST["beschreibung"]) ) {
     die("<div class='fail'> Formularfehler 1 </div>");
 }
 
@@ -38,15 +38,15 @@ if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bi
     echo "<div class='fail'> Datenbankfehler 1 </div>";
 }
 
-if (!isset($_POST["zustand"]) or !isset($_POST["length"])) {
+if (!isset($_POST["zustand"])) {
     die("<div class='fail'> Zustand eintrag falsch </div>");
 }
 
-if (!isset($_POST["preis"])  or !isset($_POST["length"])) {
+if (!isset($_POST["preis"]) ) {
     die("<div class='fail'> Preis eintrag falsch </div>");
 }
 
-if (!isset($_POST["ort"])or !isset($_POST["length"])) {
+if (!isset($_POST["ort"])) {
     die("<div class='fail'> Ort eintrag falsch </div>");
 }
 
