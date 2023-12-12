14a0904846c5bb6229cd6440f510d97d71c4d727
Alina Kell <alina.kell41@gmail.com>
2023-12-12 22:22:46 +0100

    Update community_do.php

--- .idea/Community/community_do.php
+++ .idea/Community/community_do.php
@@ -26,22 +26,14 @@ $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
 // Feld sollen nicht freigelassen werden:
 
 if(($_POST["beitrag"]) !=null){
-    
-   //festgelegtes Datum darf nicht in der Vergangenheit liegen 
-    $currentDate = date('Y-m-d');
-    $inputDate = htmlspecialchars($_POST["datum"]);
-    if(strtotime($inputDate) >= strtotime($currentDate)){
-
 
         if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
             echo "<div class='fine'> Ereignis gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Aktivitäten</a> </div>";
         } else {
             die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
         }
-    } else {
-        die("<div class='fail'> Fehlgeschlagen: Das eingegebene Datum darf nicht in der Vergangenheit liegen. <br><br>" . "<a href='aktivitaeten.php'>Erneut versuchen</a> </div>");
-    }
-}
+    } 
+
 
 
 ?>
