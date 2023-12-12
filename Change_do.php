029af37dd7fec8b50046ee77214be22eb42631e3
fb106 <147589337+fb106@users.noreply.github.com>
2023-12-12 17:16:34 +0100

    Update Change_do.php

---
+++ Change_do.php
@@ -0,0 +1,39 @@
+<?php
+include "Header Sicherheit.php";
+include "Datenbank Verbindung.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Bearbeiten</title>
+    <link rel="stylesheet" href="Übersicht.css">
+</head>
+
+<body>
+<br><br>
+<h1> Bearbeiten</h1>
+<br><br>
+
+<?php
+if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $beschreibung=$_POST['beschreibung'];
+    $zustand=$_POST['zustand'];
+    $preis=$_POST['preis'];
+    $ort=$_POST['ort'];
+    $statement=$pdo->prepare("UPDATE Upload SET beschreibung=?, zustand=?, preis=?, ort=? WHERE ID=?");
+    if ($statement->execute([$beschreibung, $zustand, $preis, $ort, $id])){
+        header('Location: Change.php');
+    }else{
+        echo "<div class='fail'>Fehlermeldung!</div>";
+        echo $statement->errorInfo()[2];
+        die();
+    }
+}
+?>
+
+</body>
+</html>
