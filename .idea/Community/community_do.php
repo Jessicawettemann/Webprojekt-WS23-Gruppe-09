5f5aa7b8270fd9f330e7f63c2aea31af09b58e15
Alina Kell <alina.kell41@gmail.com>
2023-12-12 14:14:51 +0100

    Update community_do.php

--- .idea/Community/community_do.php
+++ .idea/Community/community_do.php
@@ -33,7 +33,7 @@ $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
 // Feld sollen nicht freigelassen werden:
 
 
-if(($_POST["thema"]) !=null){
+if(($_POST["beitrag"]) !=null){
 
 
        if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
