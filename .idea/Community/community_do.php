a768503ad5e5f15ef0b6488ff58bb01aa187575d
Alina Kell <alina.kell41@gmail.com>
2023-12-13 08:59:32 +0100

    Update community_do.php

--- .idea/Community/community_do.php
+++ .idea/Community/community_do.php
@@ -22,13 +22,13 @@ session_start();
 <?php
 
 //Beitrag eintragen
-$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, Nutzer) VALUES (?,?)");
+$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag) VALUES (?)");
 
 
 // Feld sollen nicht freigelassen werden:
 if(($_POST["beitrag"]) !=null){
 
-       if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),htmlspecialchars($_POST["Nutzer"]),))){
+       if($statement->execute(array(htmlspecialchars($_POST["beitrag"]),))){
            echo "<div class='fine'> Beitrag gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
        } else {
            die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
