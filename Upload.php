dd7a24cfa9308052b2060ef7b65690f7bb2fd41e
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-19 07:47:19 +0100

    Upload

--- Upload.php
+++ Upload.php
@@ -1,46 +1,43 @@
 <?php
 include "Datenbank Verbindung.php";
-
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" type="text/css" href="Formulare.css">
     <title>Uploads</title>
 </head>
 <body>
 
+
 <?php
 if(!isset($_SESSION["Nutzer_ID"])){
-
     echo("<div class='fail'> Bitte melde dich zunächst an! "."<br><br>". "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
 }else{
-?>
+    ?>
 
-    <form1 action="Upload_do.php" method="post" enctype="multipart/form-data">
+    <form action="Upload_do.php" method="post" enctype="multipart/form-data">
         <h1> Upload</h1>
         <label class="label" for="beschreibung"></label><br>
         <input type="text" name="beschreibung" placeholder="Beschreibung">
-
+        <br>
         <label class="label" for="foto"></label><br>
         <input type="file" name="foto" placeholder="Foto">
-
+        <br>
         <label class="label" for="zustand"></label><br>
         <input type="text" name="zustand" placeholder="Zustand">
-
+        <br>
         <label class="label" for="preis"></label><br>
         <input type="text" name="preis" placeholder="Preis">
-
+        <br>
         <label class="label" for="ort"></label><br>
         <input type="text" name="ort" placeholder="Ort">
-        <br>
-
+        <br><br><br>
         <button class="button" type="submit">Hochladen</button>
-    </form1>
+    </form>
+
 
     <?php
 }
@@ -48,6 +45,3 @@ if(!isset($_SESSION["Nutzer_ID"])){
 
 </body>
 </html>
-
-
-
