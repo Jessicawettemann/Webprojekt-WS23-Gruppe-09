35616c7f81dd408de0796001b2ec17e7ef68b36a
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-19 08:04:00 +0100

    Upload mit Stysheet

--- Upload.php
+++ Upload.php
@@ -1,4 +1,3 @@
-
 <?php
 include "Datenbank Verbindung.php";
 session_start();
@@ -12,28 +11,38 @@ session_start();
     <title>Uploads</title>
 </head>
 <body>
+
+
 <?php
 if(!isset($_SESSION["Nutzer_ID"])){
     echo("<div class='fail'> Bitte melde dich zunächst an! "."<br><br>". "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
 }else{
     ?>
+
     <form1 action="Upload_do.php" method="post" enctype="multipart/form-data">
         <h1> Upload</h1>
         <label class="label" for="beschreibung"></label><br>
         <input type="text" name="beschreibung" placeholder="Beschreibung">
+        <br>
         <label class="label" for="foto"></label><br>
         <input type="file" name="foto" placeholder="Foto">
+        <br>
         <label class="label" for="zustand"></label><br>
         <input type="text" name="zustand" placeholder="Zustand">
+        <br>
         <label class="label" for="preis"></label><br>
         <input type="text" name="preis" placeholder="Preis">
+        <br>
         <label class="label" for="ort"></label><br>
         <input type="text" name="ort" placeholder="Ort">
-        <br>
+        <br><br><br>
         <button class="button" type="submit">Hochladen</button>
     </form1>
+
+
     <?php
 }
 ?>
+
 </body>
 </html>
