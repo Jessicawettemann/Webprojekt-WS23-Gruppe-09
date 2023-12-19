7bcfaa55e3d3bcb0fb3d84c059490920ba4b4dec
fb106 <147589337+fb106@users.noreply.github.com>
2023-12-19 18:05:47 +0100

    Hinzufügen neuer Foto Option

--- Upload_do.php
+++ Upload_do.php
@@ -38,6 +38,31 @@ if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bi
     echo "<div class='fail'> Datenbankfehler 1 </div>";
 }
 
+// Überprüfung, ob ein zusätzliches Bild hochgeladen wurde
+if (isset($_FILES["optionalImage"]["tmp_name"]) && isset($_FILES["optionalImage"]["name"])) {
+        // Überprüfen Sie den Dateityp und die Dateigröße
+        $optionalImageFileName = $_FILES["optionalImage"]["name"];
+        $optionalImageFileInfo = pathinfo($optionalImageFileName);
+        $optionalImageFileType = $optionalImageFileInfo['extension'];
+        $optionalImageFileSize = $_FILES["optionalImage"]["size"];
+    
+        // Überprüfungen
+        if ($optionalImageFileType == "jpg" or $optionalImageFileType == "png" or $optionalImageFileType == "PNG" or $optionalImageFileType == "pdf" or $optionalImageFileType == "HEIC" or $optionalImageFileType == "jpeg" or $optionalImageFileType == "JPG") {
+            echo "";
+        } else {
+            die("<div class='fail'> Dieses Dateiformat wird nicht unterstützt. </div>");
+        }
+    
+        if ($optionalImageFileSize > 80000000) {
+            die("<div class='fail'> Diese Datei ist leider zu groß </div>");
+        }
+    
+        // Verschiebung zusätzliche Bild in den gewünschten Ordner
+        if (!move_uploaded_file($_FILES["optionalImage"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["optionalImage"]["name"])) {
+            echo "<div class='fail'> Datenbankfehler 1 </div>";
+        }
+    }
+
 if (!isset($_POST["zustand"])) {
     die("<div class='fail'> Zustand eintrag falsch </div>");
 }
@@ -52,8 +77,8 @@ if (!isset($_POST["ort"])) {
 
 if ((!empty($_POST["beschreibung"])) and (!empty($_FILES["foto"])) and (!empty($_POST["zustand"])) and (!empty($_POST["preis"])) and (!empty($_POST["ort"]))) {
 
-    $statement = $pdo->prepare("INSERT INTO Upload (beschreibung, foto, zustand, preis, ort) VALUES (?, ?, ?, ?, ?)");
-    if ($statement->execute(array(htmlspecialchars($_POST["beschreibung"]), htmlspecialchars($_FILES["foto"]["name"]), htmlspecialchars($_POST["zustand"]), htmlspecialchars($_POST["preis"]), htmlspecialchars($_POST["ort"])))) {
+    $statement = $pdo->prepare("INSERT INTO Upload (beschreibung, foto, optionalImage, zustand, preis, ort) VALUES (?, ?, ?, ?, ?)");
+    if ($statement->execute(array(htmlspecialchars($_POST["beschreibung"]), htmlspecialchars($_FILES["foto"]["name"]), htmlspecialchars($_FILES["optionalImage"]["name"]), htmlspecialchars($_POST["zustand"]), htmlspecialchars($_POST["preis"]), htmlspecialchars($_POST["ort"])))) {
         echo "<div class='fine'> Eintrag wurde erstellt" . "<br><br>" . "<a href='Upload.php'>weiteres Angebot hochladen</a>" . "<br><br>" . "<a href='ich-biete_Übersicht.php'>Zurück zur Übersicht</a> </div>";
     } else {
         echo "<div class='fail'>Datenbank-Fehler 3</div>";
