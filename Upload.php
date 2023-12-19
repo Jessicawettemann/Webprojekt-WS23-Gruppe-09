c6c8d31476a4f9b216fd5b053cd38a223e297a89
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-19 07:59:08 +0100

    Upload mit Stysheet

--- Upload.php
+++ Upload.php
@@ -1,7 +1,9 @@
 <?php
 include "Datenbank Verbindung.php";
+
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -11,37 +13,46 @@ session_start();
     <title>Uploads</title>
 </head>
 <body>
+
+
 <?php
 if(!isset($_SESSION["Nutzer_ID"])){
+
     echo("<div class='fail'> Bitte melde dich zunächst an! "."<br><br>". "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
 }else{
     ?>
 
-    <form action="Upload_do.php" method="post" enctype="multipart/form-data">
-        <form1 action="Upload_do.php" method="post" enctype="multipart/form-data">
+    <form1 action="Upload_do.php" method="post" enctype="multipart/form-data">
             <h1> Upload</h1>
             <label class="label" for="beschreibung"></label><br>
             <input type="text" name="beschreibung" placeholder="Beschreibung">
+
             <br>
             <label class="label" for="foto"></label><br>
             <input type="file" name="foto" placeholder="Foto">
+
             <br>
             <label class="label" for="zustand"></label><br>
             <input type="text" name="zustand" placeholder="Zustand">
+
             <br>
             <label class="label" for="preis"></label><br>
             <input type="text" name="preis" placeholder="Preis">
+
             <br>
             <label class="label" for="ort"></label><br>
             <input type="text" name="ort" placeholder="Ort">
-            <br><br><br>
+            <br>
+
+            <br>
             <button class="button" type="submit">Hochladen</button>
-         </form>
+
     </form1>
 
 
     <?php
 }
 ?>
+
 </body>
 </html>
