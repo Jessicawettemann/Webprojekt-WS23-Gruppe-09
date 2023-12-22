242cec0c9071f36b4e0a2696d2717f6791a76364
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-22 15:12:43 +0100

    stylimg

--- Change.php
+++ Change.php
@@ -2,18 +2,28 @@
 include "Header Sicherheit.php";
 include "Datenbank Verbindung.php";
 session_start();
-
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="Formulare.css">
+    <title>Bearbeiten</title>
+</head>
+<body>
+<?php
 $statement=$pdo->prepare("SELECT * FROM Upload WHERE ID=?");
 if ($statement->execute(array($_GET["ID"]))){
     if($row=$statement->fetch()){
         ?>
-        <form action="Change_do.php?ID=<?php echo $row["ID"];?>" method="post" enctype="multipart/form-data">
+        <form1 action="Change_do.php?ID=<?php echo $row["ID"];?>" method="post" enctype="multipart/form-data">
             neuer Beschreibung: <input type="text" name="beschreibung" value="<?php echo $row["beschreibung"];?>"> <br> <br>
             neue Zustand: <input type="text" name="zustand" value="<?php echo $row["zustand"];?>"> <br><br>
             neue Bilddatei einfügen: <input type="file" name="foto" value="<?php echo $row["foto"];?>"> <br><br>
             neuer Preis einfügen: <input type="text" name="preis" value="<?php echo $row["preis"];?>" > <br><br>
             <input type="submit">
-        </form>
+        </form1>
         <?php
     }else{
         die("<div class='fail'>Song nicht vorhanden</div>");
