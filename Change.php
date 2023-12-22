8859245ada837d3aacee78ee3ad756a2b3ac2e9f
Jessicawettemann <jw170@hdm-stuttgart.de>
2023-12-22 15:17:03 +0100

    stylimg

--- Change.php
+++ Change.php
@@ -18,9 +18,9 @@ if ($statement->execute(array($_GET["ID"]))){
     if($row=$statement->fetch()){
         ?>
         <form1 action="Change_do.php?ID=<?php echo $row["ID"];?>" method="post" enctype="multipart/form-data">
-            <input type="text" placeholder="Neue Beschreibung" name="beschreibung" value="<?php echo $row["beschreibung"];?>"> <br> <br>
-             <input type="text" placeholder="Neuer Zustand" name="zustand" value="<?php echo $row["zustand"];?>"> <br><br>
-            <input type="file" name="foto" value="<?php echo $row["foto"];?>"> <br><br>
+            <input type="text" placeholder="Neue Beschreibung" name="beschreibung" value="<?php echo $row["beschreibung"];?>">
+             <input type="text" placeholder="Neuer Zustand" name="zustand" value="<?php echo $row["zustand"];?>"> <
+            <input type="file" name="foto" value="<?php echo $row["foto"];?>">
              <input type="text" placeholder="Neuer Preis" name="preis" value="<?php echo $row["preis"];?>" > <br><br>
             <input type="submit">
         </form1>
