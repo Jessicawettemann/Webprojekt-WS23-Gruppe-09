6f3986a55472f3559835eb6aabab4dc96d356d24
Alina Kell <alina.kell41@gmail.com>
2023-12-24 13:59:41 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -1,4 +1,5 @@
 <?php
+
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
@@ -17,19 +18,20 @@ session_start();
 
 <?php
 session_start();
+require 'header Sicherheit.php';
 
-if (!isset($_SESSION['benutzername'])) {
-    header('Location: Login Formular.php');
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
 }
 
 if (isset($_POST['beitrag'])) {
     $beitrag = $_POST['beitrag'];
 
-    $stmt = $pdo->prepare('INSERT INTO Beitrag (benutzername, beitrag) VALUES (?, ?)');
-    $stmt->execute([$_SESSION['benutzername'], $beitrag]);
+    $stmt = $pdo->prepare('INSERT INTO beitraege (username, beitrag) VALUES (?, ?)');
+    $stmt->execute([$_SESSION['username'], $beitrag]);
 }
 
-$Beitrag = $pdo->query('SELECT * FROM Beitrag ORDER BY id DESC')->fetchAll();
+$beitraege = $pdo->query('SELECT * FROM beitraege ORDER BY id DESC')->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +42,7 @@ $Beitrag = $pdo->query('SELECT * FROM Beitrag ORDER BY id DESC')->fetchAll();
     <title>Community</title>
 </head>
 <body>
-    <h1>Willkommen, <?php echo $_SESSION['bentuzername']; ?>!</h1>
+    <h1>Willkommen, <?php echo $_SESSION['username']; ?>!</h1>
 
     <form action="community.php" method="post">
         <textarea name="beitrag" rows="5" cols="50" placeholder="Gib hier deinen Beitrag ein..."></textarea>
@@ -50,12 +52,12 @@ $Beitrag = $pdo->query('SELECT * FROM Beitrag ORDER BY id DESC')->fetchAll();
 
     <table border="1">
         <tr>
-            <th>Benutzername</th>
+            <th>Username</th>
             <th>Beitrag</th>
         </tr>
-        <?php foreach ($Beitrag as $beitrag): ?>
+        <?php foreach ($beitraege as $beitrag): ?>
             <tr>
-                <td><?php echo $beitrag['benutzername']; ?></td>
+                <td><?php echo $beitrag['username']; ?></td>
                 <td><?php echo $beitrag['beitrag']; ?></td>
             </tr>
         <?php endforeach; ?>
