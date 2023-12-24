f90edf39ada09e1ebcf862b0356aabf728b66520
Alina Kell <alina.kell41@gmail.com>
2023-12-24 13:43:08 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -1,7 +1,36 @@
 <?php
-include "Header Sicherheit.php";
 include "Datenbank Verbindung.php";
+include "Header Sicherheit.php";
+session_start();
+?>
 
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width">
+
+
+   <title>Forum</title>
+</head>
+<body>
+
+<?php
+session_start();
+
+if (!isset($_SESSION['benutzername'])) {
+    header('Location: Login Formular.php');
+}
+
+if (isset($_POST['beitrag'])) {
+    $beitrag = $_POST['beitrag'];
+
+    $stmt = $pdo->prepare('INSERT INTO beitraege (username, beitrag) VALUES (?, ?)');
+    $stmt->execute([$_SESSION['username'], $beitrag]);
+}
+
+$beitraege = $pdo->query('SELECT * FROM beitraege ORDER BY id DESC')->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -9,59 +38,30 @@ include "Datenbank Verbindung.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Forum</title>
-    <link rel="stylesheet" type="text/css" href="Formulare_1.css">
+    <title>Community</title>
 </head>
 <body>
+    <h1>Willkommen, <?php echo $_SESSION['username']; ?>!</h1>
 
-
-
-    
-    <!-- Forum -->
-    <div id="forum">
-        <!-- Hier wird das Forum angezeigt -->
-    </div>
-    <!-- Formular zum Hinzufügen von Beiträgen -->
-    <form action="community_do.php" method="post" enctype="multipart/form-data">
-        <h1>Forum</h1>
-        <br><br>
-        <label for="beitrag"></label>
-        <input type="text" placeholder="Beitrag" id="beitrag" name="beitrag" required>
-
-        <button type="submit">Ereignis hinzufügen</button>
-
-    <br>
-    <br>
-    <br><br><br><br>
-
-
-<?php 
-// Daten aus der Datenbank abrufen
-$statement = $pdo->prepare("SELECT * FROM Beitrag");
-$statement->execute();
-$result = $statement->fetchAll();
-
-// Überschrift für die Tabelle
-
-echo "<table border='1'>
-<br><br><br>
-<tr>
-<th>Beitrag</th>
-<th>Datum</th>
-</tr>";
-
-// Daten aus der Datenbank durchlaufen und in die Tabelle einfügen
-foreach ($result as $row) {
-    echo "<tr>";
-    echo "<td>" . $row['beitrag'] . "</td>";
-    echo "<td>" . $row['datum'] . "</td>";
-    echo "</tr>";
-}
-
-// Tabellenende
-echo "</table>";
-        
- ?>       
+    <form action="community.php" method="post">
+        <textarea name="beitrag" rows="5" cols="50" placeholder="Gib hier deinen Beitrag ein..."></textarea>
+        <br>
+        <input type="submit" value="Beitrag erstellen">
     </form>
+
+    <table border="1">
+        <tr>
+            <th>Username</th>
+            <th>Beitrag</th>
+        </tr>
+        <?php foreach ($beitraege as $beitrag): ?>
+            <tr>
+                <td><?php echo $beitrag['username']; ?></td>
+                <td><?php echo $beitrag['beitrag']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <a href="logout.php">Logout</a>
 </body>
 </html>
