cfaccc7ce20b62152e6a8bb4dd02d5bbc1d624c3
Alina Kell <alina.kell41@gmail.com>
2023-12-24 18:40:59 +0100

    Update community_do.php

--- Community/community_do.php
+++ Community/community_do.php
@@ -3,7 +3,7 @@
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
-$benutzername = $_SESSION['benutzername'];
+
 ?>
 
 <!DOCTYPE html>
@@ -11,32 +11,26 @@ $benutzername = $_SESSION['benutzername'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-
     <title>Forum</title>
 </head>
 <body>
 
 <?php
+// Beiträge eintragen
+$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, benutzername) VALUES (?, ?)");
 
+// Überprüfen, ob das Feld nicht freigelassen wird
+if(isset($_POST["beitrag"]) && isset($_POST["benutzername"])) {
+    $beitrag = htmlspecialchars($_POST["beitrag"]);
+    $benutzername = $_POST["benutzername"];
 
-//Beiträge eintragen
-
-
-$statement = $pdo->prepare("INSERT INTO Beitrag (benutzername, beitrag) VALUES (?, ?)");
-
-// Feld sollen nicht freigelassen werden:
-
-if(($_POST["beitrag"]) !=null){
-
-
-    if($statement->execute(array($benutzername, htmlspecialchars($_POST["beitrag"]),))){
-            echo "<div class='fine'> Ereignis gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
-        } else {
-            die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
-        }
+    if($statement->execute(array($beitrag, $benutzername))) {
+        echo "<div class='fine'> Ereignis gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
+    } else {
+        die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+    }
 }
-
-
 ?>
+
 </body>
 </html>
