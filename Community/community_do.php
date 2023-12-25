8a5679334833e26f1fdfa1e08613bed53cba6ddb
Alina Kell <alina.kell41@gmail.com>
2023-12-25 13:00:17 +0100

    Update community_do.php

--- Community/community_do.php
+++ Community/community_do.php
@@ -1,16 +1,11 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-// Restlicher Code ...
-
-
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
-session_start();
+
+// Überprüfen, ob die Sitzung bereits gestartet wurde
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Beitrag in die Datenbank einfügen
 $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, benutzername) VALUES (?, ?)");
