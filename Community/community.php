308c3b5f96d715fd151a1e8528678be9c52521e4
Alina Kell <alina.kell41@gmail.com>
2023-12-25 12:51:35 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -1,53 +1,59 @@
 <?php
-include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
+include "Datenbank Verbindung.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
+    <link rel="stylesheet" type="text/css" href="Formulare_1.css">
 </head>
 <body>
 
-<h1>Community</h1>
+<!-- Formular zum Hinzufügen von Beiträgen -->
+<form action="community_do.php" method="post" enctype="multipart/form-data">
+    <h1>Forum</h1>
+    <br><br>
+    <label for="beitrag"></label>
+    <input type="text" placeholder="Beitrag" id="beitrag" name="beitrag" required>
 
-<form action="community_do.php" method="post">
-    <textarea name="beitrag" id="beitrag" rows="5" cols="50" placeholder="Gib hier deinen Beitrag ein..."></textarea><br>
-    <input type="submit" value="Beitrag senden">
+    <button type="submit">Beitrag hinzufügen</button>
+
+    <br><br><br><br>
+
+    <!-- Tabelle für Beiträge -->
+    <?php
+    // Daten aus der Datenbank abrufen
+    $statement = $pdo->prepare("SELECT Beitrag.*, Nutzer.benutzername, Nutzer.profilbild FROM Beitrag JOIN Nutzer ON Beitrag.benutzername = Nutzer.benutzername ORDER BY Beitrag.datum ASC");
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+    // Überschrift für die Tabelle
+    echo "<table border='1'>
+          <tr>
+          <th>Beitrag</th>
+          <th>Datum</th>
+          <th>Nutzer</th>
+          <th>Profilbild</th>
+          </tr>";
+
+    // Daten aus der Datenbank durchlaufen und in die Tabelle einfügen
+    foreach ($result as $row) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row['beitrag']) . "</td>";
+        echo "<td>" . $row['datum'] . "</td>";
+        echo "<td>" . $row['benutzername'] . "</td>";
+        echo "<td><img src='data:image/png;base64," . base64_encode($row['profilbild']) . "' alt='Profilbild' width='50' height='50'></td>";
+        echo "</tr>";
+    }
+
+    // Tabellenende
+    echo "</table>";
+    ?>
 </form>
-
-<?php
-
-
-// Alle Beiträge aus der Datenbank auslesen
-$statement = $pdo->prepare("SELECT * FROM Beitrag INNER JOIN Nutzer ON Beitrag.benutzername = Nutzer.benutzername");
-$statement->execute();
-
-// Beiträge in einer Tabelle anzeigen
-echo "<table border='1'>";
-echo "<tr>";
-echo "<th>Beitrag</th>";
-echo "<th>Datum</th>";
-echo "<th>Nutzer</th>";
-echo "</tr>";
-
-// Durch alle Beiträge iterieren
-foreach ($statement as $row) {
-    echo "<tr>";
-    echo "<td>" . $row['beitrag'] . "</td>";
-    echo "<td>" . $row['datum'] . "</td>";
-    echo "<td>" . $row['vorname'] . " " . $row['nachname'] . "</td>";
-    echo "</tr>";
-}
-
-// Tabellenende
-echo "</table>";
-
-?>
 
 </body>
 </html>
