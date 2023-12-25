31fb160e0666ddf1ef2cf81cf465831abac191f1
Alina Kell <alina.kell41@gmail.com>
2023-12-26 00:03:51 +0100

    Update community_do.php

--- Community/community_do.php
+++ Community/community_do.php
@@ -7,27 +7,34 @@ error_reporting(E_ALL);
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 
-// Beitrag speichern
-$statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, benutzername, profilbild) VALUES (?, ?, ?)");
+// Überprüfen, ob das Formular abgeschickt wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Beitrag speichern
+    $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, benutzername, profilbild) VALUES (?, ?, ?)");
 
-// Annahme: $_SESSION["benutzername"] enthält den aktuellen Benutzernamen
-$benutzername = $_SESSION["benutzername"];
+    // Annahme: $_SESSION["benutzername"] enthält den aktuellen Benutzernamen
+    $benutzername = $_SESSION["benutzername"];
 
-// Annahme: Das Profilbild ist in der Tabelle "Nutzer" in der Spalte "profilbild" als BLOB gespeichert
-$statementProfilbild = $pdo->prepare("SELECT profilbild FROM Nutzer WHERE benutzername = ?");
-$statementProfilbild->execute([$benutzername]);
-$profilbildRow = $statementProfilbild->fetch(PDO::FETCH_ASSOC);
+    // Annahme: Das Profilbild ist in der Tabelle "Nutzer" in der Spalte "profilbild" als BLOB gespeichert
+    $statementProfilbild = $pdo->prepare("SELECT profilbild FROM Nutzer WHERE benutzername = ?");
+    $statementProfilbild->execute([$benutzername]);
+    $profilbildRow = $statementProfilbild->fetch(PDO::FETCH_ASSOC);
 
-// Überprüfen, ob das Profilbild vorhanden ist, bevor Sie es verwenden
-if ($profilbildRow && isset($profilbildRow['profilbild'])) {
-    $profilbild = $profilbildRow['profilbild'];
-} else {
-    $profilbild = null; // Setzen Sie einen Standardwert oder NULL, falls kein Profilbild vorhanden ist
-}
+    // Überprüfen, ob das Profilbild vorhanden ist, bevor Sie es verwenden
+    if ($profilbildRow && isset($profilbildRow['profilbild'])) {
+        $profilbild = $profilbildRow['profilbild'];
+    } else {
+        $profilbild = null; // Setzen Sie einen Standardwert oder NULL, falls kein Profilbild vorhanden ist
+    }
 
-if ($statement->execute(array(htmlspecialchars($_POST["beitrag"]), $benutzername, $profilbild))) {
-    echo "<div class='fine'> Ereignis gespeichert </div>". "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
-} else {
-    die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+    if ($statement->execute(array(htmlspecialchars($_POST["beitrag"]), $benutzername, $profilbild))) {
+        echo "<div class='fine'> Ereignis gespeichert </div>" . "<br><br>" . "<a href='community.php'>Zu den Beiträgen</a> </div>";
+        
+        // Deaktiviere das Formular nach dem Absenden, um doppelte Einreichungen zu verhindern
+        echo "<script>document.getElementById('communityForm').disabled = true;</script>";
+        
+    } else {
+        die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+    }
 }
 ?>
