faad0a243d690df20cb68553faec1283539bafba
Alina Kell <alina.kell41@gmail.com>
2023-12-25 13:22:26 +0100

    Update community_do.php

--- Community/community_do.php
+++ Community/community_do.php
@@ -1,5 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+//normaler Code
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
