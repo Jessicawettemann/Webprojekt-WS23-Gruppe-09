bcb606bb78b665cc2b512ef870612c1e3ac78420
Alina Kell <alina.kell41@gmail.com>
2023-12-25 12:51:37 +0100

    Update community_do.php

--- Community/community_do.php
+++ Community/community_do.php
@@ -1,16 +1,14 @@
 <?php
-
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
 
-// Beitrag speichern
+// Beitrag in die Datenbank einfügen
 $statement = $pdo->prepare("INSERT INTO Beitrag (beitrag, benutzername) VALUES (?, ?)");
 
-if ($statement->execute(array(htmlspecialchars($_POST["beitrag"]), $_SESSION["benutzername"]))) {
-    header("Location: community.php");
+if ($statement->execute(array(htmlspecialchars($_POST["beitrag"]), $_SESSION['benutzername']))) {
+    echo "<div class='fine'>Beitrag gespeichert</div><br><br><a href='community.php'>Zu den Beiträgen</a>";
 } else {
-    die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='community.php'>Erneut versuchen</a> </div>");
+    die("<div class='fail'>Fehlgeschlagen.<br><br><a href='community.php'>Erneut versuchen</a></div>");
 }
-
 ?>
