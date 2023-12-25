c96534ff2098b3a8210e2d38c07a4f098c29c2ce
Alina Kell <alina.kell41@gmail.com>
2023-12-25 11:52:42 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -1,41 +1,68 @@
-<?php include "Header Sicherheit.php"; include "Datenbank Verbindung.php"; session_start(); ?>
+<?php
+include "Header Sicherheit.php";
+
+?>
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Terminkalender</title>
+    <title>Forum</title>
     <link rel="stylesheet" type="text/css" href="Formulare_1.css">
 </head>
 <body>
 
-<!-- Forum -->
-<div id="forum">
 
-</div>
 
-<!-- Formular zum Hinzufügen von Beiträgen -->
-<?php
-// Überprüfen, ob der Benutzer angemeldet ist
-if(isset($_SESSION['benutzername'])) {
-    echo '<form action="community_do.php" method="post" enctype="multipart/form-data">';
-    echo '    <h1>Forum</h1>';
-    echo '    <br><br>';
-    echo '    <label for="beitrag"></label>';
-    echo '    <input type="text" placeholder="Beitrag" id="beitrag" name="beitrag" required>';
-    // Verstecktes Feld für den Benutzernamen
-    echo '    <input type="hidden" name="benutzername" value="' . $_SESSION['benutzername'] . '">';
-    echo '    <button type="submit">Ereignis hinzufügen</button>';
-    echo '<br><br><br><br>';
-    echo '</form>';
-} else {
-    echo '<p>Du musst angemeldet sein, um einen Beitrag hinzuzufügen.</p>';
+    
+    <!-- Forum -->
+    <div id="forum">
+    
+    </div>
+    <!-- Formular zum Hinzufügen von Beiträgen -->
+    <form action="community_do.php" method="post" enctype="multipart/form-data">
+        <h1>Forum</h1>
+        <br><br>
+        <label for="beitrag"></label>
+        <input type="text" placeholder="Beitrag" id="beitrag" name="beitrag" required>
+
+        <button type="submit">Beitrag hinzufügen</button>
+
+    <br>
+    <br>
+    <br><br><br><br>
+
+
+<?php 
+// Daten aus der Datenbank abrufen
+$statement = $pdo->prepare("SELECT * FROM Beitrag ORDER BY datum ASC");
+$statement->execute();
+$result = $statement->fetchAll();
+
+// Überschrift für die Tabelle
+
+echo "<table border='1'>
+<br><br><br>
+<tr>
+<th>Beitrag</th>
+<th>Datum</th>
+<th>Nutzer</th>
+</tr>";
+
+// Daten aus der Datenbank durchlaufen und in die Tabelle einfügen
+foreach ($result as $row) {
+    echo "<tr>";
+    echo "<td>" . $row['beitrag'] . "</td>";
+    echo "<td>" . $row['datum'] . "</td>";
+    echo "<td>" . $row['Nutzer'] . "</td>";
+    echo "</tr>";
 }
-?>
 
-<?php
-// Rest des Codes bleibt unverändert
-?>
+// Tabellenende
+echo "</table>";
+        
+ ?>       
+    </form>
 </body>
 </html>
