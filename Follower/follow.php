d58b6e9a4f33554d6c107e9a9a5dbcabf8741ea4
Alina Kell <alina.kell41@gmail.com>
2023-12-28 20:09:33 +0100

    Update follow.php

--- Follower/follow.php
+++ Follower/follow.php
@@ -2,23 +2,23 @@
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 
-// Annahme: $_SESSION["benutzername"] enthält den aktuellen Benutzernamen
-$benutzername = $_SESSION["benutzername"];
+// Überprüfen, ob das Formular abgeschickt wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Annahme: $_POST["followed_username"] enthält den Benutzernamen, dem gefolgt werden soll
+    $followedUsername = htmlspecialchars($_POST["followed_username"]);
 
-// Annahme: $_GET["followed_username"] enthält den Benutzernamen, dem gefolgt werden soll
-$followedUsername = $_GET["followed_username"];
+    // Überprüfen, ob der Benutzer bereits folgt
+    $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
+    $checkStatement->execute([$_SESSION["benutzername"], $followedUsername]);
 
-// Überprüfen, ob der Benutzer bereits folgt
-$checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
-$checkStatement->execute([$benutzername, $followedUsername]);
-
-if ($checkStatement->rowCount() == 0) {
-    // Der Benutzer folgt noch nicht, füge ihn hinzu
-    $insertStatement = $pdo->prepare("INSERT INTO Follower (follower_username, followed_username) VALUES (?, ?)");
-    $insertStatement->execute([$benutzername, $followedUsername]);
-    echo "Du folgst jetzt " . $followedUsername;
-} else {
-    // Der Benutzer folgt bereits
-    echo "Du folgst bereits " . $followedUsername;
+    if ($checkStatement->rowCount() == 0) {
+        // Der Benutzer folgt noch nicht, füge ihn hinzu
+        $insertStatement = $pdo->prepare("INSERT INTO Follower (follower_username, followed_username) VALUES (?, ?)");
+        $insertStatement->execute([$_SESSION["benutzername"], $followedUsername]);
+        echo "Du folgst jetzt " . $followedUsername;
+    } else {
+        // Der Benutzer folgt bereits
+        echo "Du folgst bereits " . $followedUsername;
+    }
 }
 ?>
