08973bc1ed5d78eaebbb46caa60c05b6d8a42886
Alina Kell <alina.kell41@gmail.com>
2023-12-28 20:09:36 +0100

    Update unfollow.php

--- Follower/unfollow.php
+++ Follower/unfollow.php
@@ -2,23 +2,23 @@
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 
-// Annahme: $_SESSION["benutzername"] enthält den aktuellen Benutzernamen
-$benutzername = $_SESSION["benutzername"];
+// Überprüfen, ob das Formular abgeschickt wurde
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Annahme: $_POST["followed_username"] enthält den Benutzernamen, dem nicht mehr gefolgt werden soll
+    $followedUsername = htmlspecialchars($_POST["followed_username"]);
 
-// Annahme: $_GET["followed_username"] enthält den Benutzernamen, dem nicht mehr gefolgt werden soll
-$followedUsername = $_GET["followed_username"];
+    // Überprüfen, ob der Benutzer bereits folgt
+    $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
+    $checkStatement->execute([$_SESSION["benutzername"], $followedUsername]);
 
-// Überprüfen, ob der Benutzer bereits folgt
-$checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
-$checkStatement->execute([$benutzername, $followedUsername]);
-
-if ($checkStatement->rowCount() > 0) {
-    // Der Benutzer folgt, entferne ihn
-    $deleteStatement = $pdo->prepare("DELETE FROM Follower WHERE follower_username = ? AND followed_username = ?");
-    $deleteStatement->execute([$benutzername, $followedUsername]);
-    echo "Du folgst nicht mehr " . $followedUsername;
-} else {
-    // Der Benutzer folgt nicht
-    echo "Du folgst nicht " . $followedUsername;
+    if ($checkStatement->rowCount() > 0) {
+        // Der Benutzer folgt, entferne ihn
+        $deleteStatement = $pdo->prepare("DELETE FROM Follower WHERE follower_username = ? AND followed_username = ?");
+        $deleteStatement->execute([$_SESSION["benutzername"], $followedUsername]);
+        echo "Du folgst nicht mehr " . $followedUsername;
+    } else {
+        // Der Benutzer folgt nicht
+        echo "Du folgst nicht " . $followedUsername;
+    }
 }
 ?>
