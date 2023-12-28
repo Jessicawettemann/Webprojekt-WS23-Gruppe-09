90b9ea13c9343a845de0142ed11c7b773afe0d25
Alina Kell <alina.kell41@gmail.com>
2023-12-28 19:17:41 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["followed_username"])) 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" type="text/css" href="Formulare_1.css">
+    <link rel="stylesheet" type="text/css">
     <title>Forum</title>
 </head>
 <body>
