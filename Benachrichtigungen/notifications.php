e13ce271c6407100b7159d1b0079c1dfa438cd42
Alina Kell <alina.kell41@gmail.com>
2023-12-29 13:21:26 +0100

    Update notifications.php

--- Benachrichtigungen/notifications.php
+++ Benachrichtigungen/notifications.php
@@ -2,10 +2,25 @@
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 
-// Benachrichtigungen abrufen
-$notificationStatement = $pdo->prepare("SELECT * FROM Benachrichtigungen WHERE empfaenger_username = ?");
-$notificationStatement->execute([$_SESSION["benutzername"]]);
+// Holen Sie Benachrichtigungen für den angemeldeten Benutzer
+$benutzername = $_SESSION["benutzername"];
+$notificationsStatement = $pdo->prepare("SELECT * FROM Benachrichtigungen WHERE benutzername = ? AND gelesen = 0");
+$notificationsStatement->execute([$benutzername]);
+$notifications = $notificationsStatement->fetchAll(PDO::FETCH_ASSOC);
 
+// Markieren Sie die Benachrichtigungen als gelesen
+$markAsReadStatement = $pdo->prepare("UPDATE Benachrichtigungen SET gelesen = 1 WHERE benutzername = ?");
+$markAsReadStatement->execute([$benutzername]);
+
+// Versenden von E-Mail-Benachrichtigungen
+foreach ($notifications as $notification) {
+    $empfaenger = $notification['email'];
+    $betreff = 'Neue Benachrichtigung';
+    $nachricht = 'Sie haben eine neue Benachrichtigung: ' . $notification['text'];
+    
+    // Senden Sie die E-Mail
+    mail($empfaenger, $betreff, $nachricht);
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,20 +28,22 @@ $notificationStatement->execute([$_SESSION["benutzername"]]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" type="text/css">
     <title>Benachrichtigungen</title>
+    <!-- Fügen Sie hier Ihren Stylesheet-Link ein -->
 </head>
 <body>
 
 <h1>Benachrichtigungen</h1>
 
 <?php
-// Benachrichtigungen anzeigen
-while ($notification = $notificationStatement->fetch(PDO::FETCH_ASSOC)) {
-    echo "<div>";
-    echo "<p>Benachrichtigung: " . $notification['nachricht'] . "</p>";
-    echo "<p>Von: " . $notification['absender_username'] . "</p>";
-    echo "</div>";
+if (count($notifications) > 0) {
+    echo "<ul>";
+    foreach ($notifications as $notification) {
+        echo "<li>" . $notification['text'] . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "<p>Keine neuen Benachrichtigungen</p>";
 }
 ?>
 
