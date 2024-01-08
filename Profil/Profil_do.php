61dfeb1cbf127c2de8a941d56a1e0fa1a0f8876a
Jessicawettemann <jw170@hdm-stuttgart.de>
2024-01-08 16:40:47 +0100

    stylimg

--- Profil/Profil_do.php
+++ Profil/Profil_do.php
@@ -42,7 +42,7 @@ if(!isset($_SESSION["Nutzer_ID"])){
 
     echo "<div class='rows'>";
     echo "<h5> Mein Profil </h5>";
-    echo "<br><br><br>". " <p style= text-align:center </p>";
+    echo "<br><br>". " <p style= text-align:center </p>";
     echo "<img src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $row["profilbild"] . "'height='100px'> <br><br>".
         "<a href= 'Profilbild.php'>" . "Profilbild ändern". "</a>";
 
