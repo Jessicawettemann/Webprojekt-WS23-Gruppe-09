fb3086f7375c9824f4836b580ce2ef9c2e27a9dc
Jessicawettemann <jw170@hdm-stuttgart.de>
2024-01-08 16:15:15 +0100

    stylimg

--- Registrierung Neu/Registrierung_Formular.php
+++ Registrierung Neu/Registrierung_Formular.php
@@ -16,7 +16,7 @@ include"Datenbank Verbindung.php";
 <body>
 
 <div>
-    <form action="Registrierung_do.php" method="post" enctype="multipart/form-data">
+    <form1 action="Registrierung_do.php" method="post" enctype="multipart/form-data">
         <h1>Registrieren</h1>
         <label class="label" for="profilbild"></label>
         <input type="file" placeholder="Profilbild" name="profilbild">
@@ -37,7 +37,7 @@ include"Datenbank Verbindung.php";
         <input type="password" placeholder="Passwort" name="passwort">
 
         <button type="submit">Registrieren</button>
-    </form>
+    </form1>
 </div>
 
 </body>
