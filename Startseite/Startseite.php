654b28eeb0119b4f7546d786004930ee679dd22a
Jessicawettemann <jw170@hdm-stuttgart.de>
2024-01-08 14:20:53 +0100

    stylimg

--- Startseite/Startseite.php
+++ Startseite/Startseite.php
@@ -14,7 +14,8 @@ include "Header Sicherheit.php";
     <br><br><br><br><br><br><br><br><br><br><br><br>
     <h1>Willkommen  </h1>
     <p> bei Landify </p><br><br><br>
-    <button type="button" onclick="ich-biete_Übersicht.php">Finde mehr heraus</button>
+
+    <button type="button" onclick="window.location.href='ich-biete_Übersicht.php';">Finde mehr heraus!</button>
 
 </section>
 <body>
