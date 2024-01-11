a52b70b285e69b0f4100cc02608bf26086f0ee54
Alina Kell <alina.kell41@gmail.com>
2024-01-11 20:11:01 +0100

    Update aktivitaeten.php

--- Aktivitäten/aktivitaeten.php
+++ Aktivitäten/aktivitaeten.php
@@ -1,5 +1,6 @@
 <?php
 include "Header Sicherheit.php";
+include "Datenbank Verbindung.php";
 ?>
 
 <!DOCTYPE html>
@@ -12,23 +13,15 @@ include "Header Sicherheit.php";
 </head>
 <body>
 
-<!-- Vorheriger und Nächster Monat Buttons -->
-<div style="text-align: center; margin-top: 20px;">
-    <button onclick="loadPreviousMonth()">Vorheriger Monat</button>
-    <button onclick="loadNextMonth()">Nächster Monat</button>
-</div>
-
 <!-- Kalenderbereich -->
 <div id="calendar">
-<?php include "calendar.php"; ?>
-    <!-- Hier wird der Kalender angezeigt -->
+    <?php include "calendar.php"; ?>
 </div>
 
 <!-- Formular zum Hinzufügen von Ereignissen -->
 <form action="aktivitaeten_do.php" method="post" enctype="multipart/form-data">
     <h1>Kalender</h1>
     <br><br>
-    <!-- Weitere Eingabefelder für Ereignisse hier einfügen -->
     <label for="thema"></label>
     <input type="text" placeholder="Thema" id="thema" name="thema" required>
 
@@ -44,34 +37,10 @@ include "Header Sicherheit.php";
     <br>
     <br>
     <button type="submit">Ereignis hinzufügen</button>
+
+    <br>
+    <br>
 </form>
-
-<!-- JavaScript für die Monatsnavigation -->
-<script>
-    let currentDate = new Date(); // Aktuelles Datum
-
-    // Funktion zum Laden des vorherigen Monats
-    function loadPreviousMonth() {
-        currentDate.setMonth(currentDate.getMonth() - 1);
-        loadCalendar();
-    }
-
-    // Funktion zum Laden des nächsten Monats
-    function loadNextMonth() {
-        currentDate.setMonth(currentDate.getMonth() + 1);
-        loadCalendar();
-    }
-
-    // Funktion zum Laden des Kalenders basierend auf dem aktuellen Datum
-    function loadCalendar() {
-        // Hier kannst du die Logik implementieren, um den Kalender für das aktuelle Datum zu laden
-        // und die Ereignisse für den aktuellen Monat anzeigen
-        // Tipp: Du kannst Ajax verwenden, um Daten vom Server zu laden oder die Seite neu zu rendern
-    }
-
-    // Initialen Kalender laden
-    loadCalendar();
-</script>
 
 </body>
 </html>
