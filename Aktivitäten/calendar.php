da6dd119d17ec9d062d40200e83cd37d7ffa91a6
Alina Kell <alina.kell41@gmail.com>
2024-01-11 18:41:49 +0100

    Update calendar.php

--- Aktivitäten/calendar.php
+++ Aktivitäten/calendar.php
@@ -12,23 +12,12 @@ echo "<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</t
 
 $dayCounter = 1;
 
-echo "<tr>";
-for ($i = 1; $i < $firstDayOfWeek; $i++) {
-    echo "<td></td>";
-}
-
-for ($i = $firstDayOfWeek; $i <= 7; $i++) {
-    $currentDate = "$year-$month-" . str_pad($dayCounter, 2, '0', STR_PAD_LEFT);
-    echo "<td>$dayCounter</td>";
-    $dayCounter++;
-}
-
-echo "</tr>";
-
-for ($i = 2; $i <= 6; $i++) {
+for ($i = 0; $i < 6; $i++) {
     echo "<tr>";
     for ($j = 1; $j <= 7; $j++) {
-        if ($dayCounter <= $daysInMonth) {
+        if ($i === 0 && $j < $firstDayOfWeek) {
+            echo "<td></td>";
+        } elseif ($dayCounter <= $daysInMonth) {
             $currentDate = "$year-$month-" . str_pad($dayCounter, 2, '0', STR_PAD_LEFT);
             echo "<td>$dayCounter</td>";
             $dayCounter++;
