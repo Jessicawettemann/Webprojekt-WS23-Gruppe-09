95dab6909fa269ddf32796f29f0bf268b9bb4e26
fb106 <147589337+fb106@users.noreply.github.com>
2024-01-16 17:03:32 +0100

    Create Informationen.php

---
+++ Startseite/Informationen.php
@@ -0,0 +1,25 @@
+<?php
+include "Header Sicherheit.php";
+?>
+
+<!DOCTYPE html>
+<html lang="de" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Startseite</title>
+    <link rel="stylesheet" type="text/css" href="Startseite.css">
+</head>
+<body>
+<section class="Header">
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <h1>Willkommen  </h1>
+    <p> bei Landify </p><br><br><br>
+
+    <button type="button" onclick="window.location.href='ich-biete_Übersicht.php';">Finde mehr heraus!</button>
+
+</section>
+<br><br><br><br<br<br><br>
+<body>
+    <p> Infos </p>
+</body>
+</html>
