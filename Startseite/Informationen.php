cc4d2ccade20f9e260b71972ce9dcb17f3b1c1d0
fb106 <147589337+fb106@users.noreply.github.com>
2024-01-17 14:07:01 +0100

    Update Informationen.php

--- Startseite/Informationen.php
+++ Startseite/Informationen.php
@@ -14,8 +14,12 @@ include "Header Sicherheit.php";
     <br><br><br><br><br><br><br><br><br><br><br><br>
     <h1>Willkommen  </h1>
     <p> bei Landify </p><br><br><br>
+    <div class="Informationen" readonly>Landify ist eine Plattform von Studierenden für Studierende.<br> 
+        Bei uns finden Sie nicht nur aktuelle News rund um das Studentendasein, sondern hast auch die Möglichkeit Events oder anstehende Termine mit den Nutzern zu teilen.<br> 
+        Das war aber noch nicht alles.<br>
+        Landify bietet ihren Nutzern auch die Möglichkeit Dinge aller Art anzubieten. Sei es das alte Bett, eine Lampe aus der ehemaligen WG oder gar ein WG Zimmer zur Miete.<br> 
+        <br>
+        Schaut rein und connected euch mit anderen Studierenden an eurer Hochschule!</div>
 </section>
-<br><br><br><br<br<br><br>
-    <p>lLandify ist eine Plattform von Studierenden für Studierende. Bei uns finden Sie nicht nur aktuelle News rund um das Studentendarsein, sondern hast auch die Möglichkeit Events oder anstehende Termine mit den Nutzern zu teilen. Das war aber noch nicht alles. Landify bietet ihren Nutzern auch die Möglichkeit Dinge aller Art anzubieten. Sei es das alte Bett, eine Lampe aus der ehemaligen WG oder gar ein WG Zimmer zur Miete. Schaut rein und connected euch mit anderen Studierenden an eurer Hochschule!</p>
 </body>
 </html>
