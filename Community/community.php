20b38f5c50f3e2839c6996515532b3d44ffd92d2
Alina Kell <alina.kell41@gmail.com>
2024-01-28 21:22:19 +0100

    Update community.php

--- Community/community.php
+++ Community/community.php
@@ -40,50 +40,50 @@ ini_set('display_errors', 1);
     <?php
 
     if (!isset($_SESSION["Nutzer_ID"])){
-    echo("<div class='fail'> Bitte melde dich zunächst an! " . "<br><br>" . "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
-}else {
+        echo("<div class='fail'> Bitte melde dich zunächst an! " . "<br><br>" . "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
+    } else {
 
-// Überprüfen, ob das Formular zur Nutzersuche abgeschickt wurde
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Annahme: $_POST["search_user"] enthält den eingegebenen Nutzernamen oder Vorname + Nachname
-        $searchUser = htmlspecialchars($_POST["search_user"]);
+        // Überprüfen, ob das Formular zur Nutzersuche abgeschickt wurde
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Annahme: $_POST["search_user"] enthält den eingegebenen Nutzernamen oder Vorname + Nachname
+            $searchUser = htmlspecialchars($_POST["search_user"]);
 
-        // Überprüfen, ob der Nutzer existiert
-        $searchStatement = $pdo->prepare("SELECT * FROM Nutzer WHERE benutzername = ? OR CONCAT(vorname, ' ', nachname) = ?");
-        $searchStatement->execute([$searchUser, $searchUser]);
+            // Überprüfen, ob der Nutzer existiert
+            $searchStatement = $pdo->prepare("SELECT * FROM Nutzer WHERE benutzername = ? OR CONCAT(vorname, ' ', nachname) = ?");
+            $searchStatement->execute([$searchUser, $searchUser]);
 
-        if ($searchStatement->rowCount() > 0) {
-            // Nutzer gefunden, zeige Informationen oder Follow-Button an
-            $userRow = $searchStatement->fetch(PDO::FETCH_ASSOC);
+            if ($searchStatement->rowCount() > 0) {
+                // Nutzer gefunden, zeige Informationen oder Follow-Button an
+                $userRow = $searchStatement->fetch(PDO::FETCH_ASSOC);
 
-            // Hier können Sie Informationen über den gefundenen Nutzer anzeigen
-            echo "<h2>Gefundener Nutzer:</h2>";
-            echo "Benutzername: " . $userRow['benutzername'] . "<br>";
-            echo "Vorname: " . $userRow['vorname'] . "<br>";
-            echo "Nachname: " . $userRow['nachname'] . "<br>";
+                // Hier können Sie Informationen über den gefundenen Nutzer anzeigen
+                echo "<h2>Gefundener Nutzer:</h2>";
+                echo "Benutzername: " . $userRow['benutzername'] . "<br>";
+                echo "Vorname: " . $userRow['vorname'] . "<br>";
+                echo "Nachname: " . $userRow['nachname'] . "<br>";
 
-            // Überprüfen, ob der Benutzer bereits folgt
-            $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
-            $checkStatement->execute([$_SESSION["benutzername"], $userRow['benutzername']]);
+                // Überprüfen, ob der Benutzer bereits folgt
+                $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
+                $checkStatement->execute([$_SESSION["benutzername"], $userRow['benutzername']]);
 
-            echo "<form action='follow.php' method='post'>";
-            echo "<input type='hidden' name='followed_username' value='" . $userRow['benutzername'] . "'>";
+                echo "<form action='follow.php' method='post'>";
+                echo "<input type='hidden' name='followed_username' value='" . $userRow['benutzername'] . "'>";
 
-            if ($checkStatement->rowCount() == 0) {
-                // Der Benutzer folgt noch nicht, zeige den Follow-Button
-                echo "<button type='submit'>Follow</button>";
+                if ($checkStatement->rowCount() == 0) {
+                    // Der Benutzer folgt noch nicht, zeige den Follow-Button
+                    echo "<button type='submit'>Follow</button>";
+                } else {
+                    // Der Benutzer folgt bereits, zeige den Unfollow-Button
+                    echo "<button type='submit' formaction='unfollow.php'>Unfollow</button>";
+                }
+
+                echo "</form>";
             } else {
-                // Der Benutzer folgt bereits, zeige den Unfollow-Button
-                echo "<button type='submit' formaction='unfollow.php'>Unfollow</button>";
+                // Nutzer nicht gefunden
+                echo "<p>Nutzer nicht gefunden.</p>";
             }
-
-            echo "</form>";
-        } else {
-            // Nutzer nicht gefunden
-            echo "<p>Nutzer nicht gefunden.</p>";
         }
     }
-}
 
 
     $statementBeitrag = $pdo->prepare("SELECT * FROM Beitrag INNER JOIN Nutzer ON Beitrag.benutzername = Nutzer.benutzername");
@@ -96,40 +96,42 @@ ini_set('display_errors', 1);
             echo "<div class='comment-container'>";
             echo "<div class='comment'>";
 
-            // Profilbild anzeigen
+            // Profilbild und Follow-Button anzeigen
             $statementProfilbild = $pdo->prepare("SELECT profilbild FROM Nutzer WHERE benutzername = ?");
             $statementProfilbild->execute([$row['benutzername']]);
             $profilbildRow = $statementProfilbild->fetch();
 
             // Profilbild einbetten, wenn ein Bildlink vorhanden ist
             if (!empty($profilbildRow['profilbild'])) {
-                echo "<div><img class='profilpicture' src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $profilbildRow['profilbild'] . "'></div>";
+                echo "<div class='profil-container'>";
+                echo "<img class='profilpicture' src='https://mars.iuk.hdm-stuttgart.de/~jw170/Bilder/" . $profilbildRow['profilbild'] . "' alt='Profilbild'>";
+                echo "</div>";
+
+                // Follow-Button anzeigen
+                echo "<div class='follow-container'>";
+                echo "<form action='follow.php' method='post' class='follow-form'>";
+                echo "<input type='hidden' name='followed_username' value='" . $row['benutzername'] . "'>";
+                if (!isset($_SESSION["Nutzer_ID"])) {
+                    echo "<button type='submit' disabled>Follow</button>";
+                } else {
+                    // Überprüfen, ob der Benutzer bereits folgt
+                    $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
+                    $checkStatement->execute([$_SESSION["benutzername"], $row['benutzername']]);
+                    if ($checkStatement->rowCount() == 0) {
+                        // Der Benutzer folgt noch nicht, zeige den Follow-Button
+                        echo "<button type='submit'>Follow</button>";
+                    } else {
+                        // Der Benutzer folgt bereits, zeige den Unfollow-Button
+                        echo "<button type='submit' formaction='unfollow.php'>Unfollow</button>";
+                    }
+                }
+                echo "</form>";
+                echo "</div>";
             } else {
                 echo "<div>Kein Profilbild</div>";
             }
 
-            echo "<p><strong>" . $row['vorname'] . " " . $row['nachname'] . "</strong></p>";
-            echo "<p>" . $row['beitrag'] . "</p>";
-            echo "<span>" . $row['datum'] . "</span>";
-
-
-            
-             // Hier fügen Sie den Follow-Button hinzu
-    echo "<td>";
-    // Überprüfen, ob der Benutzer bereits folgt
-    $checkStatement = $pdo->prepare("SELECT * FROM Follower WHERE follower_username = ? AND followed_username = ?");
-    $checkStatement->execute([$_SESSION["benutzername"], $row['benutzername']]);
-
-    echo "<form action='follow.php' method='post'>";
-    echo "<input type='hidden' name='followed_username' value='" . $row['benutzername'] . "'>";
-
-    if ($checkStatement->rowCount() == 0) {
-        // Der Benutzer folgt noch nicht, zeige den Follow-Button
-        echo "<button type='submit'>Follow</button>";
-    } else {
-        // Der Benutzer folgt bereits, zeige den Unfollow-Button
-        echo "<button type='submit' formaction='unfollow.php'>Unfollow</button>";
-    }
+            // ... (Rest des Codes) ...
 
             echo "</div>";
             echo "</div>";
