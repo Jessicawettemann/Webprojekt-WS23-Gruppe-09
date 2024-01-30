f42e63ce83ae968a130d839221f99b2d8f5a332b
Alina Kell <alina.kell41@gmail.com>
2024-01-30 11:34:27 +0100

    Update aktivitaeten_do.php

--- Aktivitäten/aktivitaeten_do.php
+++ Aktivitäten/aktivitaeten_do.php
@@ -1,62 +1,43 @@
 <?php
-
-
 include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width">
-
-
    <title>Kalender</title>
+   <link rel="stylesheet" type="text/css" href="css_kalender.css">
 </head>
 <body>
 
-
 <?php
-
-
 if (!isset($_SESSION["Nutzer_ID"])){
-    echo("<div class='fail'> Bitte melde dich zunächst an! " . "<br><br>" . "<a href='Login Formular.php'>Hier geht's zum Login</a> </div>");
-}else {
+    echo "<div class='fail'>Bitte melde dich zunächst an! <br><a href='Login Formular.php'>Hier geht's zum Login</a></div>";
+} else {
+    if (isset($_POST["thema"], $_POST["beschreibung"], $_POST["datum"], $_POST["ort"])) {
+        $thema = htmlspecialchars($_POST["thema"]);
+        $beschreibung = htmlspecialchars($_POST["beschreibung"]);
+        $datum = htmlspecialchars($_POST["datum"]);
+        $ort = htmlspecialchars($_POST["ort"]);
 
-
-//Aktivität eintragen
-
-
-    $statement = $pdo->prepare("INSERT INTO Aktivitäten (thema, beschreibung, datum,ort) VALUES (?,?,?,?)");
-
-
-// Feld sollen nicht freigelassen werden:
-
-
-    if (($_POST["thema"]) != null and ($_POST["beschreibung"]) != null and ($_POST["datum"]) != null and ($_POST["ort"]) != null) {
-
-        //festgelegtes Datum darf nicht in der Vergangenheit liegen
         $currentDate = date('Y-m-d');
-        $inputDate = htmlspecialchars($_POST["datum"]);
-        if (strtotime($inputDate) >= strtotime($currentDate)) {
+        if (strtotime($datum) >= strtotime($currentDate)) {
+            $statement = $pdo->prepare("INSERT INTO Aktivitäten (thema, beschreibung, datum, ort) VALUES (?,?,?,?)");
 
-
-            if ($statement->execute(array(htmlspecialchars($_POST["thema"]), htmlspecialchars($_POST["beschreibung"]), htmlspecialchars($_POST["datum"]), htmlspecialchars($_POST["ort"]),))) {
-                echo "<div class='fine'> Ereignis gespeichert </div>" . "<br><br>" . "<a href='aktivitaeten.php'>Zu den Aktivitäten</a> </div>";
+            if ($statement->execute([$thema, $beschreibung, $datum, $ort])) {
+                echo "<div class='fine'>Ereignis erfolgreich gespeichert! <br><a href='aktivitaeten.php'>Zu den Aktivitäten</a></div>";
             } else {
-                die("<div class='fail'> Fehlgeschlagen." . "<br><br>" . "<a href='aktivitaeten.php'>Erneut versuchen</a> </div>");
+                echo "<div class='fail'>Fehler beim Speichern des Ereignisses. <br><a href='aktivitaeten.php'>Erneut versuchen</a></div>";
             }
         } else {
-            die("<div class='fail'> Fehlgeschlagen: Das eingegebene Datum darf nicht in der Vergangenheit liegen. <br><br>" . "<a href='aktivitaeten.php'>Erneut versuchen</a> </div>");
+            echo "<div class='fail'>Das eingegebene Datum darf nicht in der Vergangenheit liegen. <br><a href='aktivitaeten.php'>Erneut versuchen</a></div>";
         }
     }
-
 }
-
-
 ?>
 </body>
 </html>
