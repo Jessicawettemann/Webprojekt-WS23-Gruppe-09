3dfa69692190490160932a5fa694fdcf04e2bdf5
Alina Kell <alina.kell41@gmail.com>
2024-01-31 01:40:53 +0100

    Create fehlermeldung.php

---
+++ fehlermeldungen/fehlermeldung.php
@@ -0,0 +1,8 @@
+<?php
+// fehlermeldung.php
+function displayMessage($message, $messageType) {
+    echo "<div class='message-box $messageType-message' id='messageBox'>";
+    echo "<p>$message</p>";
+    echo "</div>";
+}
+?>
