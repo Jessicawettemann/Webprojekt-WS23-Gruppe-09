fd40f74046a05f88f028edb60e2a827ab0cd7f1b
fb106 <147589337+fb106@users.noreply.github.com>
2024-02-01 19:48:11 +0100

    Update notifications.php

--- Benachrichtigungen/notifications.php
+++ Benachrichtigungen/notifications.php
@@ -1,5 +1,5 @@
 <?php
-include "Datenbank Verbindung.php"; // Stellen Sie sicher, dass diese die korrekte Verbindung herstellt
+include "Datenbank Verbindung.php";
 include "Header Sicherheit.php";
 session_start();
 ?>
@@ -51,10 +51,10 @@ $notificationStatement = $pdo->prepare($notificationQuery);
 $notificationStatement->execute([$_SESSION["benutzername"]]);
 
 while ($notification = $notificationStatement->fetch(PDO::FETCH_ASSOC)) {
-echo "<div class='notification'>"; // Änderung hier, um die Klasse hinzuzufügen
+echo "<div class='notification'>"; 
 echo "<p>Benachrichtigung: Neuer Beitrag </p>";
-echo "<p>Datum des Beitrags: " . $notification['beitrag_datum'] . "</p>"; // Hier das Datum des Beitrags hinzufügen
-echo "<p>Von: " . $notification['absender_username'] . "</p>";
+echo "<p>Datum des Beitrags: " . $notification['beitrag_datum'] . "</p>"; // Hier wird das  Datum des Beitrags hinzufügt (Aus der Datenbank)
+echo "<p>Von: " . $notification['absender_username'] . "</p>"; // Hier wird der Username des Absenders bspw. fb106 hinzugefügt (Aus der Datenbank)
 echo "</div>";
 }
 ?>
