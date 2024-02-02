08fb8c805bd01a9d9c7ba90a0d0141d68329f69b
Alina Kell <alina.kell41@gmail.com>
2024-02-02 13:11:49 +0100

    Update ich-biete_Hinzufügen_do.php

--- Startseite/ich-biete_Hinzufügen_do.php
+++ Startseite/ich-biete_Hinzufügen_do.php
@@ -7,15 +7,20 @@ session_start();
 <html lang="de">
 <head>
     <title>Angebot Hinzufügen</title>
+    <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
 </head>
 <body>
 
 <?php
 if (!isset($_SESSION["Nutzer_ID"])) {
-    die("<div class='fail'>Diese Funktion steht nur angemeldeten Nutzern zu! "."<br><br>". "<a href='../Login Formular.php'>Hier geht's zum Login</a> </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Diese Funktion steht nur angemeldeten Nutzern zu Verfügung. <br><a href='Login Formular.php'>Hier geht's zum Login</a>", 'fail');
 }
 if (!isset($_FILES["Foto"]["tmp_name"]) || !isset($_FILES["Foto"]["name"])) {      ///Wieso steht hier ? Was macht es ?
-    die("<div class='fail'> Upload fehlgeschlagen </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Upload fehlgeschlagen", 'fail');
 }
 if (!empty($_FILES["Foto"]["name"])){
     $fileName = $_FILES["foto"]["name"];
@@ -25,29 +30,44 @@ if (!empty($_FILES["Foto"]["name"])){
     if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "HEIC" or $fileType == "jpeg" or $fileType == "JPG") {
         echo "";
     } else {
-        die("<div class='fail'> Dieses Dateiformat wird nicht unterstützt. </div>");
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Dieses Dateiformat wird nicht unterstützt.", 'fail');
     }
     if ($_FILES["foto"]["size"] > 80000000) {
-        die("<div class='fail'> Diese Datei ist leider zu groß </div>");
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Diese Datei ist leider zu groß.", 'fail');
     }
     if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["foto"]["name"])) {
-        echo "<div class='fail'> Datenbankfehler </div>";   /// ???
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Datenbankfehler.", 'fail');
     }
 }
 
 if (!isset($_POST["name"]) or !isset($_FILES["foto"])) {
-    die("<div class='fail'> Formularfehler </div>");    /// ???
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Formularfehler.", 'fail');
 }
 $statement = $pdo->prepare("INSERT INTO Ich_biete (name, foto) VALUES (?, ?)");   /// ???
 // Feld soll nicht freigelassen werden:
 if ($_POST["name"] !=null) {
     if ($statement->execute(array(htmlspecialchars($_POST["name"]), htmlspecialchars($_FILES["foto"]["name"])))) {   /// ???
-        echo "<div class='fine'> Upload erfolgreich" . "<br><br>" . "<a href='ich-biete_Hinzufügen.php'>weitere Angebote hinzufügen</a> </div>";
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Upload erfolgreich. <br><a href='ich-biete_Hinzufügen.php'>Weitere Angebote hinzufügen</a>", 'fine');
     } else {
-        die("<div class='fail'> Datenbank-Fehler 2.0 </div>");
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Datenbank-Fehler 2.0. <br>", 'fail');
+
     }
 } else {
-    die("<div class='fail'> Das Feld Beschreibung muss ausgefüllt werden!" . "<br><br>" . "<a href='ich-biete_Hinzufügen.php'>zurück zum Formular</a> </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Das Feld Beschreibung muss ausgefüllt werden. <br><a href='ich-biete_Hinzufügen.php'>Zurück zum Formular</a>", 'fail');
 }
 ?>
 
