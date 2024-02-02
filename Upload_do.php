b224af19ed6ef4d20628364e1382f571513d8895
Alina Kell <alina.kell41@gmail.com>
2024-02-02 13:11:55 +0100

    Update Upload_do.php

--- Upload_do.php
+++ Upload_do.php
@@ -8,14 +8,20 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
 </head>
 <?php
 if (!isset($_POST["beschreibung"]) ) {
-    die("<div class='fail'> Formularfehler 1 </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Formularfehler 1 <br>", 'fail');
+
 }
 
 if (!isset($_FILES["foto"]["tmp_name"]) || !isset($_FILES["foto"]["name"])) {
-    die("<div class='fail'> Upload fehlgeschlagen </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Upload fehlgeschlagen. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 // Überprüfen Sie den Dateityp und die Dateigröße
@@ -27,15 +33,21 @@ $fileSize = $_FILES["foto"]["size"];
 if ($fileType == "jpg" or $fileType == "png" or $fileType == "PNG" or $fileType == "pdf" or $fileType == "HEIC" or $fileType == "jpeg" or $fileType == "JPG") {
     echo "";
 } else {
-    die("<div class='fail'> Dieses Dateiformat wird nicht unterstützt. </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Dieses Dateiformat wird nicht unterstützt. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 if ($fileSize > 80000000) {
-    die("<div class='fail'> Diese Datei ist leider zu groß </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Diese Datei ist leider zu groß. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 if (!move_uploaded_file($_FILES["foto"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["foto"]["name"])) {
-    echo "<div class='fail'> Datenbankfehler 1 </div>";
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Datenbankfehler 1.", 'fail');
 }
 
 // Überprüfung, ob ein zusätzliches Bild hochgeladen wurde
@@ -50,44 +62,63 @@ if (isset($_FILES["optionalImage"]["tmp_name"]) && isset($_FILES["optionalImage"
         if ($optionalImageFileType == "jpg" or $optionalImageFileType == "png" or $optionalImageFileType == "PNG" or $optionalImageFileType == "pdf" or $optionalImageFileType == "HEIC" or $optionalImageFileType == "jpeg" or $optionalImageFileType == "JPG") {
             echo "";
         } else {
-            die("<div class='fail'> Dieses Dateiformat wird nicht unterstützt. </div>");
+            //displayMessage-Funktion
+            include 'fehlermeldung.php';
+            displayMessage("Dieses Dateiformat wird nicht unterstützt. <br><a href='Upload.php'>Zurück</a>", 'fail');
         }
     
         if ($optionalImageFileSize > 80000000) {
-            die("<div class='fail'> Diese Datei ist leider zu groß </div>");
+            //displayMessage-Funktion
+            include 'fehlermeldung.php';
+            displayMessage("Diese Datei ist leider zu groß. <br><a href='Upload.php'>Zurück</a>", 'fail');
         }
     
         // Verschiebung zusätzliche Bild in den gewünschten Ordner
         if (!move_uploaded_file($_FILES["optionalImage"]["tmp_name"], "/home/jw170/public_html/Bilder/" . $_FILES["optionalImage"]["name"])) {
-            echo "<div class='fail'> Datenbankfehler  optionales Bild</div>";
+            //displayMessage-Funktion
+            include 'fehlermeldung.php';
+            displayMessage("Datenbankfehler optionales Bild <br>", 'fail');
         }
     }
 
 if (!isset($_POST["zustand"])) {
-    die("<div class='fail'> Zustand eintrag falsch </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Zustand Eintrag falsch. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 if (!isset($_POST["preis"]) ) {
-    die("<div class='fail'> Preis eintrag falsch </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Preis Eintrag falsch. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 if (!isset($_POST["ort"])) {
-    die("<div class='fail'> Ort eintrag falsch </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Ort Eintrag falsch. <br><a href='Upload.php'>Zurück</a>", 'fail');
 }
 
 if ((!empty($_POST["beschreibung"])) and (!empty($_FILES["foto"])) and (!empty($_POST["zustand"])) and (!empty($_POST["preis"])) and (!empty($_POST["ort"]))) {
 
     $statement = $pdo->prepare("INSERT INTO Upload (beschreibung, foto, optionalImage, zustand, preis, ort) VALUES (?, ?, ?, ?, ?,?)");
     if ($statement->execute(array(htmlspecialchars($_POST["beschreibung"]), htmlspecialchars($_FILES["foto"]["name"]), htmlspecialchars($_FILES["optionalImage"]["name"]), htmlspecialchars($_POST["zustand"]), htmlspecialchars($_POST["preis"]), htmlspecialchars($_POST["ort"])))) {
-        echo "<div class='fine'> Eintrag wurde erstellt" . "<br><br>" . "<a href='Upload.php'>weiteres Angebot hochladen</a>" . "<br><br>" . "<a href='ich-biete_Übersicht.php'>Zurück zur Übersicht</a> </div>";
+            //displayMessage-Funktion
+            include 'fehlermeldung.php';
+            displayMessage("Eintrag wurde erstellt. <br><a href='ich-biete_Übersicht'>Zurück zur Übersicht</a>", 'fine');
+        
     } else {
-        echo "<div class='fail'>Datenbank-Fehler 3</div>";
-        echo $statement->errorInfo()[2];
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Datenbank-Fehler 3. <br>", 'fail');
     }
 
 
 } else {
-    die("<div class='fail'> Alle Felder müssen ausgefüllt sein!" . "<br><br>" . "<a href='Upload.php'>zurück zum Formular</a> </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Alle Felder müssen ausgefüllt sein. <br><a href='Upload.php'>Zurück</a>", 'fine');
+    
 }
 ?>
 
