39a20cf67e2250e6dbcbe01d173a03eeed4fcb39
Alina Kell <alina.kell41@gmail.com>
2024-02-02 13:44:40 +0100

    Update Passwort_do.php

--- Profil/Passwort_do.php
+++ Profil/Passwort_do.php
@@ -9,6 +9,7 @@ session_start();
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
     <title>Passwort bearbeiten</title>
 
 </head>
@@ -21,9 +22,15 @@ $hash= password_hash($_POST["passwort"].$p, PASSWORD_BCRYPT);
 
 # Sicherstellung, dass alle für das Editieren notwendigen Felder ausgefüllt sind
 if (empty($_POST["passwort"])) {
-    die("<div class='fail'>Bitte fülle für die Änderung alle Felder aus</div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Bitte fülle für die Änderung alle Felder aus.<br>", 'fail');
+    
+
 } else if(empty($_GET["Nutzer_Id"])) {
-    die("<div class='fail'>Es ist ein Problem bei der Bearbeitung aufgetreten. Bitte lade die Seite neu.</div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Es ist ein Problem bei der Bearbeitung aufgetreten. Bitte lade die Seite neu.<br>", 'fail');
 }
 
 # Änderung der Nutzerdaten in der Datenbank
@@ -33,9 +40,14 @@ $Nutzer_Id= htmlspecialchars($_GET["Nutzer_Id"], ENT_QUOTES);
 $statement->execute([$hash, $Nutzer_Id]);
 
 if($statement->execute()){
-    echo '<p>'. "<div class='fine''>Deine Änderung war erfolgreich!</div>";
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Deine Änderung war erfolgreich. <br>", 'fine');
+
 }else{
-    echo '<p>'. "<div class='fail'>Beim Bearbeiten ist etwas schiefgelaufen, bitte versuche es erneut.</div>". "</p> <a href= 'Passwort.php'> Passwort bearbeiten</a>";
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Beim Bearbeiten ist etwas schiefgelaufen, bitte versuche es erneut. <br><a href='Passwort.php'>Passwort bearbeiten</a>", 'fail');
 }
 ?>
 </body>
