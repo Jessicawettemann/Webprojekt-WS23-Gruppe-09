6c818e9fe6e499b9cda36805c7a7250da17185f2
Jessicawettemann <jw170@hdm-stuttgart.de>
2024-02-02 16:02:26 +0100

    Design

--- Profil/benutzername.php
+++ Profil/benutzername.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["Nutzer_ID"])){
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-
+    <link rel="stylesheet" type="text/css" href="Profil_1.css">
     <title>Username bearbeiten</title>
 
 </head>
--- Profil/email_do.php
+++ Profil/email_do.php
@@ -8,6 +8,7 @@ session_start();
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="Profil_1.css">
     <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
     <title>E- Mail bearbeiten</title>
 
