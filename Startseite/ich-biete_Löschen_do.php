69efb497ef18abfe9274cb2bc5babdacf25ca7c4
Alina Kell <alina.kell41@gmail.com>
2024-02-02 13:11:52 +0100

    Update ich-biete_Löschen_do.php

--- Startseite/ich-biete_Löschen_do.php
+++ Startseite/ich-biete_Löschen_do.php
@@ -8,19 +8,28 @@ session_start();
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
     <title>Angebote löschen</title>
 </head>
 <body>
 <?php
 if(!isset($_SESSION["Nutzer_ID"])) { #prüft, ob User eingeloggt ist
-    die("<div class='fail'>Du musst eingeloggt sein, um deine Angebote löschen zu können!". "<br><br>" . "<a href='../Login Formular.php'>Hier geht's zum Login</a> </div>");
+    //displayMessage-Funktion
+    include 'fehlermeldung.php';
+    displayMessage("Du musst eingeloggt sein, um deine Angebote löschen zu können. <br><a href='Login Formular.php'>Hier geht's zum Login</a>", 'fail');
 }
 if(isset($_GET["Ich_biete_ID"])){
     $statement=$pdo->prepare("DELETE FROM Ich_biete WHERE ID=?");
     if($statement->execute(array($_GET["ID"]))){
-        echo "<div class='fine'>Angebot wurde erfolgreich gelöscht</div>";
+
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Angebot wurde erfolgreich gelöscht. <br>", 'fine');
+
     }else{
-        die ("<div class='fail'>Datenbank-Fehler</div>");
+        //displayMessage-Funktion
+        include 'fehlermeldung.php';
+        displayMessage("Datenbank-Fehler. <br>", 'fail');
     }
 }
 ?>
