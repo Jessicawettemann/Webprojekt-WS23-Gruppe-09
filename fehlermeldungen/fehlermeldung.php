ec35858c5c4b7e3fceaa4270060ec475ee2c7a98
Alina Kell <alina.kell41@gmail.com>
2024-02-02 13:17:47 +0100

    Delete fehlermeldung.php

--- fehlermeldungen/fehlermeldung.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-// fehlermeldung.php
-function displayMessage($message, $messageType) {
-    echo "<div class='message-box $messageType-message' id='messageBox'>";
-    echo "<p>$message</p>";
-    echo "</div>";
-}
-?>
