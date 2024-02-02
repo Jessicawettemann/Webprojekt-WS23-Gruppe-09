98905c17a0e10d65873e0111bc78bf99447c6a38
fb106 <147589337+fb106@users.noreply.github.com>
2024-02-02 13:15:03 +0100

    Update Change.php

--- Change.php
+++ Change.php
@@ -17,33 +17,26 @@ session_start();
 $statement=$pdo->prepare("SELECT * FROM Upload WHERE ID=?");
 if ($statement->execute(array($_GET["ID"]))){
     if($row=$statement->fetch()){
-        ?>
+?>
 
         <form action="Change_do.php?ID=<?php echo $row["ID"];?>" method="post" enctype="multipart/form-data">
             <h1>Bearbeiten</h1>
             <input type="text" placeholder="Neue Beschreibung" name="beschreibung" value="<?php echo $row["beschreibung"];?>">
-             <input type="text" placeholder="Neuer Zustand" name="zustand" value="<?php echo $row["zustand"];?>">
-            <input type="file" name="foto" value="<?php echo $row["foto"];?>">
-             <input type="text" placeholder="Neuer Preis" name="preis" value="<?php echo $row["preis"];?>" > <br>
+            <input type="text" placeholder="Neuer Zustand" name="zustand" value="<?php echo $row["zustand"];?>">
+            <input type="file" name="foto">
+            <input type="text" placeholder="Neuer Preis" name="preis" value="<?php echo $row["preis"];?>" > <br>
 
             <button type="submit">Absenden</button> <br><br>
             <a href="ich-biete_Übersicht.php">Zurück </a>
         </form>
-        <?php
-
+<?php
     }else{
         die("<div class='fail'>Beitrag schon vorhanden</div>");
     }
-
-} else{
-    die("<div class='fail'>Formular-Fehler</div>");
-}
-
-
-
+    } else{
+        die("<div class='fail'>Formular-Fehler</div>");
+    }
 ?>
-
-
 
 </body>
 </html>
