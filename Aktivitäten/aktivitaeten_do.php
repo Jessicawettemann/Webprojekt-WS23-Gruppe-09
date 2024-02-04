460769ef9109a626e9a0c8f21a1bcd618cfba8ae
Alina Kell <alina.kell41@gmail.com>
2024-02-04 17:00:13 +0100

    Update aktivitaeten_do.php

--- Aktivitäten/aktivitaeten_do.php
+++ Aktivitäten/aktivitaeten_do.php
@@ -1,11 +1,7 @@
 <?php
-// Einbinden der Datei für die Datenbankverbindung
 include "Datenbank Verbindung.php";
-
-// Einbinden der Datei für Sicherheitsüberprüfungen
 include "Header Sicherheit.php";
 
-// Starten der PHP-Session
 session_start();
 ?>
 
@@ -15,23 +11,22 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <title>Kalender</title>
-    
-    <!-- Einbinden der Stylesheets für die Darstellung -->
+
     <link rel="stylesheet" type="text/css" href="css_kalender.css">
     <link rel="stylesheet" type="text/css" href="fehlermeldung.css">
 </head>
 <body>
 
 <?php
-// Überprüfen, ob der Benutzer angemeldet ist
+// Überprüfung, ob Benutzer angemeldet ist
 if (!isset($_SESSION["Nutzer_ID"])){
     //displayMessage-Funktion für die Fehlermeldung
     include 'fehlermeldung.php';
     displayMessage("Bitte melde dich zunächst an! <br><a href='Login Formular.php'>Hier geht's zum Login</a>", 'fail');
 } else {
-    // Überprüfen, ob alle erforderlichen Felder im Formular gesetzt sind
+    // Überprüfung, ob alle erforderlichen Felder im Formular gesetzt sind
     if (isset($_POST["thema"]) && isset($_POST["beschreibung"]) && isset($_POST["datum"]) && isset($_POST["ort"])) {
-        // Daten aus dem Formular sicher verarbeiten
+        // Daten aus dem Formular verarbeiten
         $thema = htmlspecialchars($_POST["thema"]);
         $beschreibung = htmlspecialchars($_POST["beschreibung"]);
         $datum = htmlspecialchars($_POST["datum"]);
@@ -40,7 +35,7 @@ if (!isset($_SESSION["Nutzer_ID"])){
         // Aktuelles Datum erhalten
         $currentDate = date('Y-m-d');
         
-        // Überprüfen, ob das eingegebene Datum in der Zukunft liegt
+        // Überprüfung, ob eingegebene Datum in der Zukunft liegt
         if (strtotime($datum) >= strtotime($currentDate)) {
             // Vorbereiten des SQL-Statements zum Einfügen in die Datenbank
             $statement = $pdo->prepare("INSERT INTO Aktivitäten (thema, beschreibung, datum, ort) VALUES (?,?,?,?)");
@@ -56,7 +51,7 @@ if (!isset($_SESSION["Nutzer_ID"])){
                 displayMessage("Fehler beim Speichern des Ereignisses. <br><a href='aktivitaeten.php'>Erneut versuchen</a>", 'fail');
             }
         } else {
-            //displayMessage-Funktion für Fehlermeldung bei ungültigem Datum
+            //displayMessage-Funktion
             include 'fehlermeldung.php';
             displayMessage("Das eingegebene Datum darf nicht in der Vergangenheit liegen. <br><a href='aktivitaeten.php'>Erneut versuchen</a>", 'fail');
         }
